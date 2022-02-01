a434c95b22f315edaee69f6f7d0ecfe239254015
jisungbin <ji@sungb.in>
2022-02-01 11:00:23 +0900

    Check multi module hilt working

--- presentation/src/main/kotlin/team/applemango/runnerbe/activity/MainActivity.kt
+++ presentation/src/main/kotlin/team/applemango/runnerbe/activity/MainActivity.kt
@@ -9,17 +9,33 @@
 
 package team.applemango.runnerbe.activity
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import team.applemango.runnerbe.R
+import team.applemango.runnerbe.shared.util.extension.get
+import team.applemango.runnerbe.shared.util.extension.set
+import team.applemango.runnerbe.shared.util.extension.toast
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var testPref: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen() // TODO: Splash Theme
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (testPref["TEST"] == null) {
+            toast("EMPTY!")
+            testPref["TEST"] = "BYE"
+        } else {
+            toast(testPref["TEST"].orEmpty())
+        }
     }
 }
