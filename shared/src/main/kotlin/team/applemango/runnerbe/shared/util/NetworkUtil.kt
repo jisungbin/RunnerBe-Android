c2c57d246dde5e8e7170b792598ef2c253724b66
jisungbin <ji@sungb.in>
2022-02-01 10:50:59 +0900

    Fix sdk issue

--- shared/src/main/kotlin/team/applemango/runnerbe/shared/util/NetworkUtil.kt
+++ shared/src/main/kotlin/team/applemango/runnerbe/shared/util/NetworkUtil.kt
@@ -9,19 +9,30 @@
 
 package team.applemango.runnerbe.shared.util
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
+import android.os.Build
 
 object NetworkUtil {
-    @SuppressLint("MissingPermission")
+    @Suppress("DEPRECATION", "MissingPermission")
     fun isNetworkAvailable(context: Context) =
         (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).run {
-            getNetworkCapabilities(activeNetwork)?.run {
-                hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
-                    hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
-                    hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
-            } ?: false
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                getNetworkCapabilities(activeNetwork)?.run {
+                    hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                        hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                        hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+                } ?: false
+            } else {
+                val activeNetworkInfo = activeNetworkInfo
+                if (activeNetworkInfo != null) {
+                    val type = activeNetworkInfo.type
+                    if (type == ConnectivityManager.TYPE_MOBILE || type == ConnectivityManager.TYPE_WIFI) {
+                        return true
+                    }
+                }
+                return false
+            }
         }
 }
