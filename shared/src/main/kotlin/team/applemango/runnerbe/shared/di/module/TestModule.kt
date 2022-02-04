82606d772cb53b05d25580763dfcb256cde30fdd
jisungbin <ji@sungb.in>
2022-02-05 03:25:43 +0900

    di test

--- shared/src/main/kotlin/team/applemango/runnerbe/shared/di/module/TestModule.kt
+++ shared/src/main/kotlin/team/applemango/runnerbe/shared/di/module/TestModule.kt
@@ -2,7 +2,7 @@
  * RunnerBe © 2022 Team AppleMango. all rights reserved.
  * RunnerBe license is under the MIT.
  *
- * [TestBindModule.kt] created by Ji Sungbin on 22. 2. 1. 오전 11:02
+ * [TestProvideModule.kt] created by Ji Sungbin on 22. 2. 5. 오전 3:13
  *
  * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
  */
@@ -14,13 +14,13 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.scopes.ActivityScoped
-import team.applemango.runnerbe.shared.di.test.TestRepo
-import team.applemango.runnerbe.shared.di.test.TestRepoImpl
+import team.applemango.runnerbe.data.TestImpl
+import team.applemango.runnerbe.domain.Test
 
 @Module
 @InstallIn(ActivityComponent::class)
-abstract class TestBindModule {
+abstract class TestModule {
     @Binds
     @ActivityScoped
-    abstract fun bindTestRepo(impl: TestRepoImpl): TestRepo
+    abstract fun providesTestRepo(impl: TestImpl): Test
 }
