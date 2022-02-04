c38d15d0f16a44b6c49772c6bc488d16122541d5
jisungbin <ji@sungb.in>
2022-02-05 01:49:07 +0900

    Update typography

--- theme/src/main/kotlin/team/applemango/runnerbe/theme/typography.kt
+++ theme/src/main/kotlin/team/applemango/runnerbe/theme/typography.kt
@@ -9,23 +9,93 @@
 
 package team.applemango.runnerbe.theme
 
-import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.sp
 
-val RunnerBeTypography = Typography(
-    defaultFontFamily = FontFamily(Font(R.font.notosans_r)),
-    h1 =,
-    h2 =,
-    h3 =,
-    h4 =,
-    h5 =,
-    h6 =,
-    subtitle1 =,
-    subtitle2 =,
-    body1 =,
-    body2 =,
-    button =,
-    caption =,
-    overline =
-)
+private typealias font = R.font
+
+private fun Int.toFontFamily() = FontFamily(Font(this))
+
+object FontAsset {
+    val Aggro = font.aggro_l.toFontFamily()
+
+    object NotoSans {
+        val M = font.notosans_m.toFontFamily()
+        val R = font.notosans_r.toFontFamily()
+        val B = font.notosans_b.toFontFamily()
+    }
+
+    object Roboto {
+        val R = font.roboto_r.toFontFamily()
+        val M = font.roboto_m.toFontFamily()
+    }
+}
+
+object Typography {
+    val Header28M = TextStyle(
+        fontFamily = FontAsset.NotoSans.M,
+        fontSize = 28.sp,
+        lineHeight = 42.sp,
+        letterSpacing = (-0.84).sp
+    )
+    val Title16B = TextStyle(
+        fontFamily = FontAsset.NotoSans.B,
+        fontSize = 18.sp,
+        lineHeight = 26.sp,
+        letterSpacing = (-0.4).sp
+    )
+    val Title18R = TextStyle(
+        fontFamily = FontAsset.NotoSans.R,
+        fontSize = 18.sp,
+        lineHeight = 27.sp,
+        letterSpacing = (-0.11).sp
+    )
+    val Title16M = TextStyle(
+        fontFamily = FontAsset.NotoSans.M,
+        fontSize = 16.sp,
+        lineHeight = 26.sp,
+        letterSpacing = (-0.4).sp
+    )
+    val Body16R = TextStyle(
+        fontFamily = FontAsset.NotoSans.R,
+        fontSize = 16.sp,
+        lineHeight = 26.sp,
+        letterSpacing = (-0.4).sp
+    )
+    val EngBody16R = TextStyle(
+        fontFamily = FontAsset.Roboto.R,
+        fontSize = 16.sp,
+        lineHeight = 27.sp
+    )
+    val Body14B = TextStyle(
+        fontFamily = FontAsset.NotoSans.B,
+        fontSize = 14.sp,
+        lineHeight = 22.sp,
+        letterSpacing = (-0.6).sp
+    )
+    val Body14M = TextStyle(
+        fontFamily = FontAsset.NotoSans.M,
+        fontSize = 14.sp,
+        lineHeight = 22.sp,
+        letterSpacing = (-0.6).sp
+    )
+    val Body14R = TextStyle(
+        fontFamily = FontAsset.NotoSans.R,
+        fontSize = 14.sp,
+        lineHeight = 22.sp,
+        letterSpacing = (-0.4).sp
+    )
+    val EngBody14R = TextStyle(
+        fontFamily = FontAsset.Roboto.R,
+        fontSize = 14.sp,
+        lineHeight = 18.sp
+    )
+    val Body12B = TextStyle(
+        fontFamily = FontAsset.NotoSans.B,
+        fontSize = 12.sp,
+        letterSpacing = (-0.2).sp
+    )
+
+}
