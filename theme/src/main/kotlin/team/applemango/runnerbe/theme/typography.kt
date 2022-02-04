60f000820e73e506e9278dbd72030955c6fcef65
jisungbin <ji@sungb.in>
2022-02-05 00:45:33 +0900

    Update typography

---
+++ theme/src/main/kotlin/team/applemango/runnerbe/theme/typography.kt
@@ -0,0 +1,31 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [typography.kt] created by Ji Sungbin on 22. 2. 5. 오전 12:35
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+
+val RunnerBeTypography = Typography(
+    defaultFontFamily = FontFamily(Font(R.font.notosans_r)),
+    h1 =,
+    h2 =,
+    h3 =,
+    h4 =,
+    h5 =,
+    h6 =,
+    subtitle1 =,
+    subtitle2 =,
+    body1 =,
+    body2 =,
+    button =,
+    caption =,
+    overline =
+)
