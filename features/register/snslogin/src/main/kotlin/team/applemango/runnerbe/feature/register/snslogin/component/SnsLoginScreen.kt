bd26693ca1cdcc7fb9a5a3a8bef915eb814294d1
jisungbin <ji@sungb.in>
2022-02-08 04:51:24 +0900

    Fix gradient brush type error

--- features/register/snslogin/src/main/kotlin/team/applemango/runnerbe/feature/register/snslogin/component/SnsLoginScreen.kt
+++ features/register/snslogin/src/main/kotlin/team/applemango/runnerbe/feature/register/snslogin/component/SnsLoginScreen.kt
@@ -22,7 +22,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -47,7 +46,7 @@ internal fun SnsLoginScreen(vm: SnsLoginViewModel) {
     ConstraintLayout(
         modifier = Modifier
             .fillMaxSize()
-            .background(brush = Brush.linearGradient(GradientAsset.RegisterCommonBackground))
+            .background(brush = GradientAsset.RegisterCommonBackground)
             .systemBarsPadding(start = false, end = false)
             .padding(horizontal = 16.dp)
     ) {
