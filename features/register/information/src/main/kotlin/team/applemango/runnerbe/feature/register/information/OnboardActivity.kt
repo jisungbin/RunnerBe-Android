fdabd20724c8f66ee7a6cb09edff3eaf47627245
jisungbin <ji@sungb.in>
2022-02-08 01:02:37 +0900

    Create onboard activity

---
+++ features/register/information/src/main/kotlin/team/applemango/runnerbe/feature/register/information/OnboardActivity.kt
@@ -0,0 +1,10 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [OnboardActivity.kt] created by Ji Sungbin on 22. 2. 8. 오전 1:02
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.feature.register.information
