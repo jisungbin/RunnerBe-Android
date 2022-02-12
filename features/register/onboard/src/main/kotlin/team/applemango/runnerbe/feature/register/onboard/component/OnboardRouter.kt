a760bc73ec1ec16bac6a5920c7a3964630451c5a
jisungbin <ji@sungb.in>
2022-02-13 01:23:41 +0900

    Fix exception activity crash error and remove test mode option

--- features/register/onboard/src/main/kotlin/team/applemango/runnerbe/feature/register/onboard/component/OnboardRouter.kt
+++ features/register/onboard/src/main/kotlin/team/applemango/runnerbe/feature/register/onboard/component/OnboardRouter.kt
@@ -255,7 +255,7 @@ internal fun OnboardRouter(
                                 dataStore = context.dataStore,
                                 photo = photo!!, // non null, 만약 null 로 들어오면 작동되지 않아야 하기 때문에 NonNull 강제 처리
                                 nextStep = Step.VerifyWithEmployeeIdRequestDone,
-                                isTestMode = true
+                                // isTestMode = true
                             )
                         }
                     }
