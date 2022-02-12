3a401dbfe819dd21e54d7de17763557e16fcc855
jisungbin <ji@sungb.in>
2022-02-13 01:25:49 +0900

    Increase app version code

--- buildSrc/src/main/kotlin/ApplicationConstants.kt
+++ buildSrc/src/main/kotlin/ApplicationConstants.kt
@@ -14,7 +14,7 @@ object ApplicationConstants {
     const val targetSdk = 31
     const val compileSdk = 31
     const val jvmTarget = "11"
-    const val versionCode = 6
+    const val versionCode = 7
     const val versionName = "0.1.0-dev06"
     val javaVersion = JavaVersion.VERSION_11
 }
