24d8509ade499d50e27ee337b885a1d93bfbc04d
jisungbin <ji@sungb.in>
2022-02-13 23:39:27 +0900

    Update mail related files

--- domain/src/main/kotlin/team/applemango/runnerbe/domain/mail/usecase/MailSendUseCase.kt
+++ domain/src/main/kotlin/team/applemango/runnerbe/domain/mail/usecase/MailSendUseCase.kt
@@ -15,6 +15,8 @@ import team.applemango.runnerbe.domain.mail.model.MailTemplateItem
 import team.applemango.runnerbe.domain.mail.model.To
 import team.applemango.runnerbe.domain.mail.repository.MailRepository
 
+private fun buildLink(token: String) = "http://runnerbe-auth.shop/?verify=$token"
+
 class MailSendUseCase(private val repo: MailRepository) {
     suspend operator fun invoke(token: String, email: String) = runCatching {
         repo.send(
@@ -24,8 +26,14 @@ class MailSendUseCase(private val repo: MailRepository) {
                         from = From(email = "welcome@runnerbe-auth.shop", name = "RunnerBe"),
                         to = listOf(To(email = email)),
                         htmlPart = """
-                            아래 링크를 통해 인증해 주세요 ><
-                            <a href="http://runnerbe-auth.shop/verify=$token">http://runnerbe-auth.shop/verify=$token</a>
+                            안녕하세요. 팀 RunnerBe 입니다.
+                            만약 귀하께서 요청하신 인증 메일이 아니라면 이 메일을 무시하셔도 돼요.
+
+                            하단의 링크를 클릭해 이메일 주소를 인증하면 회원님에 대한 모든 소개가 완료돼요.
+
+                            <a href="${buildLink(token)}>${buildLink(token)}</a>
+                            
+                            그럼, 지금부터 러너비와 힘차게 달려볼까요?
                         """.trimIndent(),
                         subject = "러너비에 오신걸 환영해요 \uD83E\uDD73", // 🥳
                     )
