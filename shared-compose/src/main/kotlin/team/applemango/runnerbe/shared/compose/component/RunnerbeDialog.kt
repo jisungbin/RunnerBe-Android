de17f1874ffa1ea8921e7b4d8bcf08068d67c1dc
jisungbin <ji@sungb.in>
2022-02-23 02:25:54 +0900

    Refactor: add base classes

---
+++ shared-compose/src/main/kotlin/team/applemango/runnerbe/shared/compose/component/RunnerbeDialog.kt
@@ -0,0 +1,48 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [RunnerbeDialog.kt] created by Ji Sungbin on 22. 2. 23. 오전 2:20
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.shared.compose.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
+import team.applemango.runnerbe.shared.compose.theme.ColorAsset
+
+@Composable
+fun RunnerbeDialog(
+    visible: Boolean,
+    onDismissRequest: () -> Unit,
+    properties: DialogProperties = DialogProperties(),
+    content: @Composable ColumnScope.() -> Unit,
+) {
+    if (visible) {
+        CustomAlertDialog(
+            onDismissRequest = { onDismissRequest() },
+            properties = properties
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(color = ColorAsset.G5)
+            ) {
+                content()
+            }
+        }
+    }
+}
