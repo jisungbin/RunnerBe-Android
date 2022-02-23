e635d0883fcac745930c5ed0c9c6dd252f40639e
jisungbin <ji@sungb.in>
2022-02-24 05:42:27 +0900

    Add KeywordFilter class

---
+++ domain/src/main/kotlin/team/applemango/runnerbe/domain/main/constant/KeywordFilter.kt
@@ -0,0 +1,15 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [KeywordFilter.kt] created by Ji Sungbin on 22. 2. 24. 오전 5:18
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.domain.main.constant
+
+sealed class KeywordFilter(val code: String) {
+    object None : KeywordFilter("N")
+    data class Create(val keyword: String) : KeywordFilter(keyword)
+}
