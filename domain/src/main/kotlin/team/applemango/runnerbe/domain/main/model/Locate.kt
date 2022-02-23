ce0500e43d359e592ddb21a2ed4dc013992a084c
jisungbin <ji@sungb.in>
2022-02-24 05:42:41 +0900

    Add Locate model

---
+++ domain/src/main/kotlin/team/applemango/runnerbe/domain/main/model/Locate.kt
@@ -0,0 +1,16 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [Locate.kt] created by Ji Sungbin on 22. 2. 24. 오전 5:17
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.domain.main.model
+
+data class Locate(
+    val address: String,
+    val longitude: Double,
+    val latitude: Double,
+)
