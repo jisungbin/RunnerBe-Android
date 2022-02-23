575b5e81b31e9668c0050607a9ed8c0f24aca0d8
jisungbin <ji@sungb.in>
2022-02-24 00:07:19 +0900

    Create new MainBoard composable file

---
+++ features/home/board/src/main/kotlin/team/applemango/runnerbe/feature/home/board/MainBoard.kt
@@ -0,0 +1,19 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [MainBoard.kt] created by Ji Sungbin on 22. 2. 23. 오후 11:50
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.feature.home.board
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainBoard(
+    isFavoriteBoard: Boolean = false,
+) {
+
+}
