495b38b979306f6dedd0de01c82af55f8df0e0e1
jisungbin <ji@sungb.in>
2022-02-24 19:47:02 +0900

    Add header helper extension

---
+++ data/src/main/kotlin/team/applemango/runnerbe/data/util/extension/header.kt
@@ -0,0 +1,12 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [header.kt] created by Ji Sungbin on 22. 2. 24. 오후 7:35
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.data.util.extension
+
+private fun String.toXAccessTokenHeader() = mapOf("x-access-token" to this)
