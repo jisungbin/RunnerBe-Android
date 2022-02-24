247927a66006700818ac1b7268b755c29bb543bf
jisungbin <ji@sungb.in>
2022-02-24 21:25:59 +0900

    Add WriteRunningItemUseCase class

---
+++ domain/src/main/kotlin/team/applemango/runnerbe/domain/main/usecase/WriteRunningItemUseCase.kt
@@ -0,0 +1,47 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [WriteRunningItemUseCase.kt] created by Ji Sungbin on 22. 2. 24. 오후 9:01
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.domain.main.usecase
+
+import java.text.SimpleDateFormat
+import java.util.Locale
+import team.applemango.runnerbe.domain.main.model.write.RunningItemBody
+import team.applemango.runnerbe.domain.main.model.write.RunningItemBodyData
+import team.applemango.runnerbe.domain.main.repository.MainRepository
+
+class WriteRunningItemUseCase(private val repo: MainRepository) {
+    suspend operator fun invoke(
+        jwt: String,
+        userId: Int,
+        item: RunningItemBody,
+    ) = runCatching {
+        val meetingTime =
+            SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(item.meetingDate)
+        val (address, latitude, longitude) = item.locate
+
+        repo.writeRunningItem(
+            jwt = jwt,
+            userId = userId,
+            item = RunningItemBodyData(
+                title = item.title,
+                runnerGender = item.gender.code,
+                runningTag = item.itemType.code,
+                runningTime = item.runningTime,
+                gatheringTime = meetingTime,
+                locationInfo = address,
+                gatherLatitude = latitude.toString(),
+                gatherLongitude = longitude.toString(),
+                ageMin = item.ageRange.getCode { min },
+                ageMax = maxAge,
+                peopleNum = 0,
+                contents = ""
+            )
+        )
+    }
+}
