a857b395494b3fae05319484add674b5785b4bc2
jisungbin <ji@sungb.in>
2022-02-24 20:30:59 +0900

    Add DefaultResponse file

---
+++ data/src/main/kotlin/team/applemango/runnerbe/data/main/model/common/DefaultResponse.kt
@@ -0,0 +1,24 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [DefaultResponse.kt] created by Ji Sungbin on 22. 2. 24. 오후 8:24
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.data.main.model.common
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import team.applemango.runnerbe.data.common.BaseResponse
+
+data class DefaultResponse(
+    @field:JsonProperty("code")
+    override val code: Int? = null,
+
+    @field:JsonProperty("message")
+    override val message: String? = null,
+
+    @field:JsonProperty("isSuccess")
+    override val isSuccess: Boolean? = null,
+) : BaseResponse
