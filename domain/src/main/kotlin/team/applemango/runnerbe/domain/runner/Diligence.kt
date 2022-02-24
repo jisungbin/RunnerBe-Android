f337231c263d7a985cbdeb709d9f5fe63220c6c5
jisungbin <ji@sungb.in>
2022-02-25 04:50:35 +0900

    Add Diligence class

---
+++ domain/src/main/kotlin/team/applemango/runnerbe/domain/runner/Diligence.kt
@@ -0,0 +1,17 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [Diligence.kt] created by Ji Sungbin on 22. 2. 25. 오전 4:38
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.domain.runner
+
+/**
+ * 러너 성실도
+ */
+enum class Diligence(val message: String) {
+    Effort("노력 러너")
+}
