cb20ccff83c1f74317114aa30364cc3548f9088f
jisungbin <ji@sungb.in>
2022-03-01 00:13:27 +0900

    Add api implementations

--- data/src/main/kotlin/team/applemango/runnerbe/data/user/repository/UserRepositoryImpl.kt
+++ data/src/main/kotlin/team/applemango/runnerbe/data/user/repository/UserRepositoryImpl.kt
@@ -9,8 +9,13 @@
 
 package team.applemango.runnerbe.data.user.repository
 
+import team.applemango.runnerbe.data.common.isSuccessNonNull
+import team.applemango.runnerbe.data.runningitem.constant.NotYetVerifyCode
+import team.applemango.runnerbe.data.runningitem.constant.SuccessCode
+import team.applemango.runnerbe.data.user.mapper.toNicknameChangeResult
+import team.applemango.runnerbe.data.util.extension.requireSuccessfulBody
 import team.applemango.runnerbe.data.util.userApi
-import team.applemango.runnerbe.domain.user.constant.NicknameChangeResult
+import team.applemango.runnerbe.domain.runningitem.common.BaseResult
 import team.applemango.runnerbe.domain.user.model.Nickname
 import team.applemango.runnerbe.domain.user.repository.UserRepository
 
@@ -19,12 +24,18 @@ class UserRepositoryImpl : UserRepository {
         jwt: String,
         userId: Int,
         nickName: Nickname,
-    ): NicknameChangeResult {
+    ): BaseResult {
         val request = userApi.setNickname(
             jwt = jwt,
             userId = userId,
             nickname = nickName
         )
+        return request.requireSuccessfulBody(
+            requestName = "userApi.setNickname",
+            resultVerifyBuilder = { body ->
+                body.code in listOf(SuccessCode, NotYetVerifyCode, 3004, 3005)
+            }
+        ).toNicknameChangeResult()
     }
 
     override suspend fun updateBookmarkItem(
@@ -33,7 +44,18 @@ class UserRepositoryImpl : UserRepository {
         userId: Int,
         whetherAdd: String,
     ): Boolean {
-        TODO("Not yet implemented")
+        val request = userApi.updateBookmarkItem(
+            jwt = jwt,
+            postId = postId,
+            userId = userId,
+            whetherAdd = whetherAdd
+        )
+        return request.requireSuccessfulBody(
+            requestName = "userApi.updateBookmarkItem",
+            resultVerifyBuilder = { body ->
+                body.code in listOf(SuccessCode, NotYetVerifyCode)
+            }
+        ).isSuccessNonNull
     }
 
     override suspend fun loadBookmarkItems() {
