3baf93b7a5a9dbc5a2f00a5ab49ff24e841e7cc7
jisungbin <ji@sungb.in>
2022-02-28 17:52:04 +0900

    Update comments and new fields (isMyItem, bookmarked)

--- domain/src/main/kotlin/team/applemango/runnerbe/domain/runningitem/model/runningitem/information/RunningItemInformation.kt
+++ domain/src/main/kotlin/team/applemango/runnerbe/domain/runningitem/model/runningitem/information/RunningItemInformation.kt
@@ -12,7 +12,16 @@ package team.applemango.runnerbe.domain.runningitem.model.runningitem.informatio
 import team.applemango.runnerbe.domain.runningitem.model.runner.Runner
 import team.applemango.runnerbe.domain.runningitem.model.runningitem.RunningItem
 
+/**
+ * @property isMyItem 내가 올린 러닝 아이템인지 여부 (owner boolean)
+ * @property bookmarked 내가 북마크 한 러닝 아이템인지 여부
+ * @property item 러닝 아이템 상새 정보
+ * @property joinRunners 참여한 러너들
+ * @property waitingRunners 참여 신청한 러너들
+ */
 data class RunningItemInformation(
+    val isMyItem: Boolean,
+    val bookmarked: Boolean,
     val item: RunningItem,
     val joinRunners: List<Runner>,
     val waitingRunners: List<Runner>,
