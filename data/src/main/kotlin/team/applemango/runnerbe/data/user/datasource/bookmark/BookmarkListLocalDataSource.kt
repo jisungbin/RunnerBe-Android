be79e48fe4de9859ea3baa2a0dadb0aae3a1eb17
jisungbin <ji@sungb.in>
2022-02-28 16:54:44 +0900

    Add bookmark list datasource

---
+++ data/src/main/kotlin/team/applemango/runnerbe/data/user/datasource/bookmark/BookmarkListLocalDataSource.kt
@@ -0,0 +1,14 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [BookmarkListLocalDataSource.kt] created by Ji Sungbin on 22. 2. 28. 오후 4:38
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.data.user.datasource.bookmark
+
+/**
+ * TODO: 즐찾 목록만 오프라인 지원
+ */
---
+++ data/src/main/kotlin/team/applemango/runnerbe/data/user/datasource/bookmark/BookmarkListRemoteDataSource.kt
@@ -0,0 +1,10 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [BookmarkListRemoteDataSource.kt] created by Ji Sungbin on 22. 2. 28. 오후 4:40
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.data.user.datasource.bookmark
