4177e0bb04ca4d462d7b41fbd12b5a58a49cf044
jisungbin <ji@sungb.in>
2022-02-28 17:45:56 +0900

    Add DefaultResultCode constants

---
+++ data/src/main/kotlin/team/applemango/runnerbe/data/runningitem/constant/DefaultResultCode.kt
@@ -0,0 +1,13 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [DefaultResultCode.kt] created by Ji Sungbin on 22. 2. 28. 오후 5:40
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.data.runningitem.constant
+
+internal const val SuccessCode = 1000
+internal const val NotYetVerifyCode = 2044
