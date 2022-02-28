bd42dd5cb8785f138e591ba1f1f18f017e502cf4
jisungbin <ji@sungb.in>
2022-02-28 17:30:41 +0900

    Add resultCodeExceptionMessage constant

--- shared-domain/src/main/kotlin/team/applemango/runnerbe/shared/domain/ExceptionMessage.kt
+++ shared-domain/src/main/kotlin/team/applemango/runnerbe/shared/domain/ExceptionMessage.kt
@@ -2,7 +2,7 @@
  * RunnerBe © 2022 Team AppleMango. all rights reserved.
  * RunnerBe license is under the MIT.
  *
- * [requireExceptionMessage.kt] created by Ji Sungbin on 22. 2. 24. 오후 3:21
+ * [ExceptionMessage.kt] created by Ji Sungbin on 22. 2. 24. 오후 3:21
  *
  * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
  */
@@ -10,3 +10,4 @@
 package team.applemango.runnerbe.shared.domain
 
 fun requireFieldExceptionMessage(fieldName: String) = "Require `$fieldName` field is null."
+fun resultCodeExceptionMessage(code: Int) = "Result code $code is not allowed here."
