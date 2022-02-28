9f6d3fe518f33f712e7b239778e34fb0e559ca09
jisungbin <ji@sungb.in>
2022-02-28 17:17:57 +0900

    Update result code check logic

--- data/src/main/kotlin/team/applemango/runnerbe/data/register/login/mapper/LoginRequestResponseMapper.kt
+++ data/src/main/kotlin/team/applemango/runnerbe/data/register/login/mapper/LoginRequestResponseMapper.kt
@@ -16,9 +16,9 @@ import team.applemango.runnerbe.shared.domain.requireFieldExceptionMessage
 internal fun LoginRequestResponse.toDomain(): UserToken {
     val code = checkNotNull(code) { requireFieldExceptionMessage("code") }
     val loginResult = checkNotNull(loginResult) { requireFieldExceptionMessage("loginResult") }
-    check(code in listOf(1001, 1007)) { "Success result code must be 1001 or 1007." }
-    return when (code) { // must 1001, 1007
+    return when (code) {
         1001 -> UserToken(jwt = requireNotNull(loginResult.jwt) { requireFieldExceptionMessage("jwt") }) // 회원
-        else -> UserToken(uuid = requireNotNull(loginResult.uuid) { requireFieldExceptionMessage("uuid") }) // 비회원
+        1007 -> UserToken(uuid = requireNotNull(loginResult.uuid) { requireFieldExceptionMessage("uuid") }) // 비회원
+        else -> throw IllegalStateException("LoginRequestResponse result code must be 1001 or 1007")
     }
 }
