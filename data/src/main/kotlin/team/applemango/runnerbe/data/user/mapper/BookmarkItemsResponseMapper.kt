f2dcda2ea2542d3d4366c0c1d66d37051576edb9
jisungbin <ji@sungb.in>
2022-03-01 15:17:22 +0900

    Refactor: apply new code style

--- data/src/main/kotlin/team/applemango/runnerbe/data/user/mapper/BookmarkItemsResponseMapper.kt
+++ data/src/main/kotlin/team/applemango/runnerbe/data/user/mapper/BookmarkItemsResponseMapper.kt
@@ -2,7 +2,7 @@
  * RunnerBe © 2022 Team AppleMango. all rights reserved.
  * RunnerBe license is under the MIT.
  *
- * [LoadBookmarkItemsResponseMapper.kt] created by Ji Sungbin on 22. 3. 1. 오전 10:16
+ * [BookmarkItemsResponseMapper.kt] created by Ji Sungbin on 22. 3. 1. 오전 10:16
  *
  * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
  */
@@ -17,6 +17,8 @@ import team.applemango.runnerbe.shared.domain.requireFieldNullMessage
 internal fun BookmarkItemsResponse.toDomain() = requireNotNull(result) {
     requireFieldNullMessage("result")
 }.bookMarkList?.map { item ->
-    checkNotNull(item) { requireFieldNullMessage("result") }
+    checkNotNull(item) {
+        requireFieldNullMessage("result")
+    }
     item.toDomain(MappingType.BookmarkApiFields)
 } ?: throw Exception(requireFieldNullMessage("bookMarkList"))
