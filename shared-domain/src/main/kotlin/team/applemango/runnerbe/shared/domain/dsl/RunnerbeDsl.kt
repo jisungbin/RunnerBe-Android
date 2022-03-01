a8c60254b221c00b39720a79a77cab3ac1c510e3
jisungbin <ji@sungb.in>
2022-03-01 19:58:22 +0900

    Add DslMarker annotation

---
+++ shared-domain/src/main/kotlin/team/applemango/runnerbe/shared/domain/dsl/RunnerbeDsl.kt
@@ -0,0 +1,13 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [RunnerbeDsl.kt] created by Ji Sungbin on 22. 3. 1. 오후 7:33
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.shared.domain.dsl
+
+@DslMarker
+annotation class RunnerbeDsl
