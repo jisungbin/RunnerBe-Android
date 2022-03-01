c258bd09fb30377d5dfa26bfa2aeddf035b90124
jisungbin <ji@sungb.in>
2022-03-01 15:17:31 +0900

    Change result type

--- data/src/main/kotlin/team/applemango/runnerbe/data/user/model/MyPageInformationResponse.kt
+++ data/src/main/kotlin/team/applemango/runnerbe/data/user/model/MyPageInformationResponse.kt
@@ -14,7 +14,7 @@ import team.applemango.runnerbe.data.common.BaseResponse
 
 data class MyPageInformationResponse(
     @field:JsonProperty("result")
-    val result: BookmarkItemsResult? = null,
+    val result: MyPageInformationResult? = null,
 
     @field:JsonProperty("code")
     override val code: Int? = null,
