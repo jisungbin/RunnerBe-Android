f0ffd746c2b2078b3d852baca44cc54ca9a6ae88
jisungbin <ji@sungb.in>
2022-03-01 10:39:19 +0900

    Add UserRepository.changeJob api model

---
+++ domain/src/main/kotlin/team/applemango/runnerbe/domain/user/model/JobWrapper.kt
@@ -0,0 +1,12 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [JobWrapper.kt] created by Ji Sungbin on 22. 3. 1. 오전 10:31
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.domain.user.model
+
+data class JobWrapper(val jobCode: String)
