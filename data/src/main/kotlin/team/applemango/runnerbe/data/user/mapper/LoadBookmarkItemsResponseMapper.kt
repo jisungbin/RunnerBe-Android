a352a7f23208da5dbd455c5302f43ed5575592c3
jisungbin <ji@sungb.in>
2022-03-01 10:19:45 +0900

    Add mapper

---
+++ data/src/main/kotlin/team/applemango/runnerbe/data/user/mapper/LoadBookmarkItemsResponseMapper.kt
@@ -0,0 +1,22 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [LoadBookmarkItemsResponseMapper.kt] created by Ji Sungbin on 22. 3. 1. 오전 10:16
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.data.user.mapper
+
+import team.applemango.runnerbe.data.runningitem.mapper.MappingType
+import team.applemango.runnerbe.data.runningitem.mapper.toDomain
+import team.applemango.runnerbe.data.user.model.LoadBookmarkItemsResponse
+import team.applemango.runnerbe.shared.domain.requireFieldNullMessage
+
+internal fun LoadBookmarkItemsResponse.toDomain() = requireNotNull(result) {
+    requireFieldNullMessage("result")
+}.bookMarkList?.map { item ->
+    checkNotNull(item) { requireFieldNullMessage("result") }
+    item.toDomain(MappingType.BookmarkApiFields)
+} ?: throw Exception(requireFieldNullMessage("bookMarkList"))
