40c983dd8c15ddcd98c6551253b4f2d53108a7b4
jisungbin <ji@sungb.in>
2022-03-01 14:44:55 +0900

    Add shared-domain files

---
+++ shared-domain/src/main/kotlin/team/applemango/runnerbe/shared/domain/constant/string.kt
@@ -0,0 +1,12 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [string.kt] created by Ji Sungbin on 22. 3. 1. 오후 2:30
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.shared.domain.constant
+
+const val EmptyString = ""
