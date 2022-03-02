1d2ee12842c721671ad30d7d54138ad77327d91d
jisungbin <ji@sungb.in>
2022-03-02 21:25:57 +0900

    Update unknownResultMessage function

--- shared-domain/src/main/kotlin/team/applemango/runnerbe/shared/domain/ExceptionMessage.kt
+++ shared-domain/src/main/kotlin/team/applemango/runnerbe/shared/domain/ExceptionMessage.kt
@@ -11,3 +11,4 @@ package team.applemango.runnerbe.shared.domain
 
 fun requireFieldNullMessage(fieldName: String) = "Require `$fieldName` field is null."
 fun notAllowedValueMessage(value: Any) = "$value is not allowed here."
+fun unknownResultMessage(result: Any) = "Unknown result: $result"
