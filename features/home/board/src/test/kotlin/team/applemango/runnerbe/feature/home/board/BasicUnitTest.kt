b0b413b9dc465e88caefa9f5c910642542a5db28
jisungbin <ji@sungb.in>
2022-03-10 21:33:24 +0900

    Setup basic unit tests

---
+++ features/home/board/src/test/kotlin/team/applemango/runnerbe/feature/home/board/BasicUnitTest.kt
@@ -0,0 +1,21 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [BasicUnitTest.kt] created by Ji Sungbin on 22. 1. 31. 오후 9:30
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.feature.home.board
+
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers.`is`
+import org.junit.jupiter.api.Test
+
+class BasicUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        MatcherAssert.assertThat("1", `is`("1"))
+    }
+}
