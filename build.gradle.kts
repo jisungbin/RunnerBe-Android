a4a25ca9d16f154c49fd740deda33f07e13b8db0
jisungbin <ji@sungb.in>
2022-03-12 19:01:19 +0900

    Update jacoco setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -101,5 +101,5 @@ tasks.register("clean", Delete::class) {
 apply {
     // plugin("com.vanniktech.dependency.graph.generator")
     from("gradle/projectDependencyGraph.gradle")
-    from("gradle/jacoco.gradle")
+    from("gradle/jacoco/project.gradle")
 }
