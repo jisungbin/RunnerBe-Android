28ad34a54fe22543609e2507088f00ea5ec0cc55
jisungbin <ji@sungb.in>
2022-03-14 14:09:49 +0900

    Chore: remove dokka setting

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -7,34 +7,8 @@
  * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
  */
 
-import org.jetbrains.dokka.base.DokkaBase
-import org.jetbrains.dokka.base.DokkaBaseConfiguration
-
 plugins {
     id("com.android.library")
     id("kotlin-android")
     id("org.jetbrains.dokka") version Versions.BuildUtil.Dokka
-}
-
-buildscript {
-    dependencies {
-        classpath("org.jetbrains.dokka:dokka-base:${Versions.BuildUtil.Dokka}")
-    }
-}
-
-tasks.dokkaHtml.configure {
-    moduleName.set("RunnerBe-Android: Domain")
-    pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
-        footerMessage =
-            """made with <span style="color: orange;">❤</span> by <a href="https://github.com/jisungbin">@jisungbin</a>"""
-        separateInheritedMembers = true
-    }
-    dokkaSourceSets {
-        configureEach {
-            includeNonPublic.set(true)
-            skipDeprecated.set(false)
-            skipEmptyPackages.set(false)
-            jdkVersion.set(11)
-        }
-    }
 }
