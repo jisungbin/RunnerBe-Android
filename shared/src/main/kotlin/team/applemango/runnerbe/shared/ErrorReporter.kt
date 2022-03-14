d2c4ed47b590283f61c81e36b81ab8e16de7a33e
jisungbin <ji@sungb.in>
2022-03-14 12:40:43 +0900

    Create ErrorReporter class

---
+++ shared/src/main/kotlin/team/applemango/runnerbe/shared/ErrorReporter.kt
@@ -0,0 +1,12 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [ErrorReporter.kt] created by Ji Sungbin on 22. 3. 14. 오후 12:39
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.shared
+
+class ErrorReporter
