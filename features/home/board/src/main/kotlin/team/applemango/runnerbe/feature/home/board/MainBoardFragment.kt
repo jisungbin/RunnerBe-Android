2af6693deae602fbbb01246639d95f95b5cd9e6c
jisungbin <ji@sungb.in>
2022-03-15 19:30:04 +0900

    Add MainBoard fragment

---
+++ features/home/board/src/main/kotlin/team/applemango/runnerbe/feature/home/board/MainBoardFragment.kt
@@ -0,0 +1,33 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [MainBoardFragment.kt] created by Ji Sungbin on 22. 3. 15. 오후 6:48
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.feature.home.board
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+
+class MainBoardFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        return ComposeView(requireActivity()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+
+            }
+        }
+    }
+}
