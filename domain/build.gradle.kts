e4c31517d64e7ebfc97159c0592e5e4cb4f2df7e
jisungbin <ji@sungb.in>
2022-03-15 20:05:44 +0900

    Add JaCoCo dependency

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -11,8 +11,33 @@ plugins {
     id("com.android.library")
     id("kotlin-android")
     id("org.jetbrains.dokka") version Versions.BuildUtil.Dokka
+    id("de.mannodermaus.android-junit5")
+    jacoco
+}
+
+jacoco {
+    toolVersion = Versions.Test.JaCoCo
+}
+
+tasks.withType<JacocoReport> {
+    reports {
+        html.required.set(true)
+        html.outputLocation.set(layout.projectDirectory.dir("../documents/coverage/jacoco/html"))
+        xml.required.set(true) // codecov depends on xml format report
+        xml.outputLocation.set(layout.projectDirectory.file("../documents/coverage/jacoco/report.xml"))
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    finalizedBy("jacocoTestReport")
 }
 
 dependencies {
     implementation(Dependencies.Coroutine)
+
+    testRuntimeOnly(Dependencies.Test.JunitEngine)
+    testImplementation(Dependencies.Test.JunitApi)
+    testImplementation(Dependencies.Test.Hamcrest)
+    testImplementation(Dependencies.Test.Coroutine)
 }
