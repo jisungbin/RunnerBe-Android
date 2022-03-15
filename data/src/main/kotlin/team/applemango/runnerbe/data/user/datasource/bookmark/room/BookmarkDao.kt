bb9485a361b16af602c24d2fe6b34c6b95badb60
jisungbin <ji@sungb.in>
2022-03-15 21:21:08 +0900

    Add BookmarkDao

---
+++ data/src/main/kotlin/team/applemango/runnerbe/data/user/datasource/bookmark/room/BookmarkDao.kt
@@ -0,0 +1,17 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [BookmarkDao.kt] created by Ji Sungbin on 22. 3. 15. 오후 9:06
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.data.user.datasource.bookmark.room
+
+import androidx.room.Dao
+
+@Dao
+interface BookmarkDao {
+    suspend fun
+}
