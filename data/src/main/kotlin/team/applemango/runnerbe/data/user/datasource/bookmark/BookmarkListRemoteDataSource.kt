eae1a6c949188660873a194f17871b5e3eef7cca
jisungbin <ji@sungb.in>
2022-03-15 22:18:44 +0900

    Update BookmarkListDataSource

--- data/src/main/kotlin/team/applemango/runnerbe/data/user/datasource/bookmark/BookmarkListRemoteDataSource.kt
+++ data/src/main/kotlin/team/applemango/runnerbe/data/user/datasource/bookmark/BookmarkListRemoteDataSource.kt
@@ -9,6 +9,52 @@
 
 package team.applemango.runnerbe.data.user.datasource.bookmark
 
-/**
- * TODO: api call datasource 분리
- */
+import team.applemango.runnerbe.data.common.toBaseResult
+import team.applemango.runnerbe.data.runningitem.constant.NotYetVerifyCode
+import team.applemango.runnerbe.data.runningitem.constant.SuccessCode
+import team.applemango.runnerbe.data.user.datasource.bookmark.base.BookmarkListBaseDataSource
+import team.applemango.runnerbe.data.user.mapper.toDomain
+import team.applemango.runnerbe.data.util.extension.requireSuccessfulBody
+import team.applemango.runnerbe.data.util.userApi
+import team.applemango.runnerbe.domain.runningitem.common.BaseResult
+import team.applemango.runnerbe.domain.runningitem.model.runningitem.RunningItem
+
+class BookmarkListRemoteDataSource : BookmarkListBaseDataSource {
+    override suspend fun updateBookmarkItem(
+        jwt: String,
+        postId: Int,
+        userId: Int,
+        whetherAdd: String,
+        unregisterUser: Boolean,
+    ): BaseResult {
+        val request = userApi.updateBookmarkItem(
+            jwt = jwt,
+            postId = postId,
+            userId = userId,
+            whetherAdd = whetherAdd
+        )
+        return request.requireSuccessfulBody(
+            requestName = "userApi.updateBookmarkItem",
+            resultVerifyBuilder = { body ->
+                body.code in listOf(SuccessCode, NotYetVerifyCode)
+            }
+        ).toBaseResult()
+    }
+
+    override suspend fun loadBookmarkItems(
+        jwt: String,
+        userId: Int,
+        unregisterUser: Boolean,
+    ): List<RunningItem> {
+        val request = userApi.loadBookmarkItems(
+            jwt = jwt,
+            userId = userId,
+        )
+        return request.requireSuccessfulBody(
+            requestName = "userApi.loadBookmarkItems",
+            resultVerifyBuilder = { body ->
+                body.code in listOf(SuccessCode, NotYetVerifyCode)
+            }
+        ).toDomain()
+    }
+}
