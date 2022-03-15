da6fb9f5c1679da8f3883814a645314857853a86
jisungbin <ji@sungb.in>
2022-03-15 19:45:35 +0900

    Update MainBoardState comment

--- features/home/board/src/main/kotlin/team/applemango/runnerbe/feature/home/board/mvi/MainBoardState.kt
+++ features/home/board/src/main/kotlin/team/applemango/runnerbe/feature/home/board/mvi/MainBoardState.kt
@@ -12,7 +12,7 @@ package team.applemango.runnerbe.feature.home.board.mvi
 /**
  * @property None 성공, 초기, 에러 상태는 UI 에서 따로 취할 액션이
  * 없기 때문에 None 하나로 모두 포함함
- * @property NonRegisterUser 비회원 유저로써 아직 이 기능 이용이 불기능함
+ * @property NonRegisterUser 비회원 유저로써 아직 특정 기능 이용이 불기능함
  */
 internal enum class MainBoardState {
     None,
