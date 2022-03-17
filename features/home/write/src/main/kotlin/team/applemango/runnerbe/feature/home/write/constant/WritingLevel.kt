f24665dd6d129e9c278ea6e740dbf1d210ca41f0
jisungbin <ji@sungb.in>
2022-03-18 07:13:59 +0900

    Add RunningItemWrite constant

---
+++ features/home/write/src/main/kotlin/team/applemango/runnerbe/feature/home/write/constant/WritingLevel.kt
@@ -0,0 +1,15 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [WritingStep.kt] created by Ji Sungbin on 22. 3. 18. 오전 6:49
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.feature.home.write.constant
+
+internal enum class WritingLevel(val index: Int) {
+    One(0),
+    Two(1)
+}
