cc49a3ca287abf4d4a6889b6865d998eb7c1644d
jisungbin <ji@sungb.in>
2022-03-18 08:57:40 +0900

    Add toDefaultDateTime function to RunningItemType enum class

--- domain/src/main/kotlin/team/applemango/runnerbe/domain/runningitem/common/RunningItemType.kt
+++ domain/src/main/kotlin/team/applemango/runnerbe/domain/runningitem/common/RunningItemType.kt
@@ -15,5 +15,16 @@ package team.applemango.runnerbe.domain.runningitem.common
  * @property Off 휴일
  */
 enum class RunningItemType(val code: String) {
-    Before("B"), After("A"), Off("H")
+    Before("B"),
+    After("A"),
+    Off("H");
+
+    /**
+     * 출근 전 - 06:00 AM, 퇴근 후 06:00 PM, 휴일 - 12:00 PM
+     */
+    fun toDefaultDateTime() = when (this) {
+        Before -> "06:00 AM"
+        After -> "06:00 PM"
+        Off -> "12:00 PM"
+    }
 }
