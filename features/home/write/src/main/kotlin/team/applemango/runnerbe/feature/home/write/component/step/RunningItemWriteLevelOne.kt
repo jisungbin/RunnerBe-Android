b1725279a27fb26052170722b82c386bdc191d87
jisungbin <ji@sungb.in>
2022-03-18 08:58:13 +0900

    Update RunningItemWriteLevelOne ui

--- features/home/write/src/main/kotlin/team/applemango/runnerbe/feature/home/write/component/step/RunningItemWriteLevelOne.kt
+++ features/home/write/src/main/kotlin/team/applemango/runnerbe/feature/home/write/component/step/RunningItemWriteLevelOne.kt
@@ -10,12 +10,18 @@
 package team.applemango.runnerbe.feature.home.write.component.step
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
@@ -23,21 +29,37 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import java.util.Date
+import team.applemango.runnerbe.domain.runningitem.common.RunningItemType
 import team.applemango.runnerbe.feature.home.write.R
+import team.applemango.runnerbe.feature.home.write.model.RunningDate
+import team.applemango.runnerbe.shared.compose.component.RunnerbeDialog
 import team.applemango.runnerbe.shared.compose.theme.ColorAsset
 import team.applemango.runnerbe.shared.compose.theme.Typography
+import team.applemango.runnerbe.shared.domain.extension.format
+
+private val DefaultFieldShape = RoundedCornerShape(6.dp)
+
+private fun Date.toDateFormat() = format("M/dd (E)")
 
 @Composable
 internal fun RunningItemWriteLevelOne(
+    runningItemType: RunningItemType,
     fieldsAllInputStateChange: (state: Boolean) -> Unit,
 ) {
     var titleField by remember { mutableStateOf(TextFieldValue()) }
     val fieldsFillState = remember { mutableStateListOf(false, false, false) }
 
     var titleErrorVisible by remember { mutableStateOf(false) }
+
+    fieldsAllInputStateChange(fieldsFillState.all { true })
 
     Column(modifier = Modifier.fillMaxSize()) {
         Text(
@@ -46,6 +68,8 @@ internal fun RunningItemWriteLevelOne(
         )
         TextField(
             value = titleField,
+            shape = DefaultFieldShape,
+            colors = TextFieldDefaults.textFieldColors(backgroundColor = ColorAsset.G5_5),
             onValueChange = { newTitleValue ->
                 if (newTitleValue.text.isNotEmpty()) {
                     fieldsFillState[0] = true
@@ -79,5 +103,73 @@ internal fun RunningItemWriteLevelOne(
             text = stringResource(R.string.runningitemwrite_label_date),
             style = Typography.Body14R.copy(color = ColorAsset.G3_5)
         )
+        ConstraintLayout(
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable {
+                }
+                .padding(horizontal = 32.dp)
+        ) {
+            val (calendarIcon, dateString, arrowIcon) = createRefs()
+            Icon(
+                modifier = Modifier
+                    .size(18.dp)
+                    .constrainAs(calendarIcon) {
+                        start.linkTo(parent.start)
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                    },
+                painter = painterResource(R.drawable.ic_round_schedule_24),
+                contentDescription = null,
+                tint = Color.Unspecified
+            )
+            Text(
+                modifier = Modifier
+                    .constrainAs(dateString) {
+                        start.linkTo(calendarIcon.end, 16.dp)
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                    }
+                    .clip(DefaultFieldShape)
+                    .background(color = ColorAsset.G5_5),
+                text = "${Date().toDateFormat()} ${runningItemType.toDefaultDateTime()}",
+                style = Typography.Body14R.copy(color = ColorAsset.G1)
+            )
+            Icon(
+                modifier = Modifier
+                    .size(18.dp)
+                    .constrainAs(arrowIcon) {
+                        start.linkTo(parent.end)
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                    },
+                painter = painterResource(R.drawable.ic_round_chevron_right),
+                contentDescription = null,
+                tint = Color.Unspecified
+            )
+        }
     }
+}
+
+@Composable
+private fun DatePickerDialog(
+    visible: Boolean,
+    onDismissRequest: () -> Unit,
+    onRunningDateChange: (runningDate: RunningDate) -> Unit,
+) {
+    RunnerbeDialog(
+        visible = visible,
+        onDismissRequest = onDismissRequest,
+        positiveButton = {
+            textBuilder = {
+                stringResource(R.string.runningitemwrite_dialog_button_decision)
+            }
+            onClick = {
+                onDismissRequest()
+            }
+        },
+        content = {
+
+        }
+    )
 }
