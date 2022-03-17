f4bb23903bfe6fa2c49f647d05cafb9e4e490063
jisungbin <ji@sungb.in>
2022-03-17 20:48:34 +0900

    Add mail tab and use constant bnv height

---
+++ shared/src/main/kotlin/team/applemango/runnerbe/shared/constant/height.kt
@@ -0,0 +1,12 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [height.kt] created by Ji Sungbin on 22. 3. 17. 오후 8:37
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.shared.constant
+
+const val BottomNavigationBarHeight = 56
