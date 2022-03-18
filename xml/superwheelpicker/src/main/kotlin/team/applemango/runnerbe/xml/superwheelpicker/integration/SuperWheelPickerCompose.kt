ac09736231d850880d09cb9fcc104c5c03c34ca7
jisungbin <ji@sungb.in>
2022-03-18 09:57:23 +0900

    Support SuperWheelPicker compose integration

---
+++ xml/superwheelpicker/src/main/kotlin/team/applemango/runnerbe/xml/superwheelpicker/integration/SuperWheelPickerCompose.kt
@@ -0,0 +1,65 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [SuperWheelPickerCompose.kt] created by Ji Sungbin on 22. 3. 18. 오전 8:53
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.xml.superwheelpicker.integration
+
+import android.graphics.Typeface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.viewinterop.AndroidView
+import team.applemango.runnerbe.xml.superwheelpicker.OnTextRenderListener
+import team.applemango.runnerbe.xml.superwheelpicker.OnValueChangeListener
+
+private typealias SuperWheelPickerXml = team.applemango.runnerbe.xml.superwheelpicker.SuperWheelPicker
+
+@Immutable
+data class SuperWheelPickerColors(
+    val selectedTextColor: Color = Color.Black,
+    val unselectedTextColor: Color = Color.LightGray,
+)
+
+@Immutable
+data class SuperWheelPickerTextStyle(
+    val typeface: Typeface? = Typeface.DEFAULT,
+)
+
+@Composable
+fun SuperWheelPicker(
+    modifier: Modifier = Modifier,
+    colors: SuperWheelPickerColors = SuperWheelPickerColors(),
+    textStyle: SuperWheelPickerTextStyle = SuperWheelPickerTextStyle(),
+    wrapSelectorWheel: Boolean = true,
+    wheelItemCount: Int = 5,
+    range: IntRange,
+    value: Int = range.last / 2,
+    onValueChange: OnValueChangeListener? = null,
+    onTextRender: OnTextRenderListener? = null,
+) = AndroidView(
+    modifier = modifier,
+    factory = { context ->
+        SuperWheelPickerXml(context).apply {
+            setSelectedTextColor(colors.selectedTextColor.toArgb())
+            setUnselectedTextColor(colors.unselectedTextColor.toArgb())
+            setTypeface(textStyle.typeface) // nullable
+            setWrapSelectorWheel(wrapSelectorWheel)
+            setWheelItemCount(wheelItemCount)
+            setRange(range)
+            setValue(value)
+            if (onValueChange != null) {
+                setOnValueChangedListener(onValueChange)
+            }
+            if (onTextRender != null) {
+                setTextRenderListener(onTextRender)
+            }
+        }
+    }
+)
