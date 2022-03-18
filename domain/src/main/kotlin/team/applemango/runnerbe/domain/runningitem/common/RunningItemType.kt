042a267dd22add9b42b0114b94d611d74775366c
jisungbin <ji@sungb.in>
2022-03-19 00:35:12 +0900

    Remove useless toDefaultDateTime function at RunningItemType

--- domain/src/main/kotlin/team/applemango/runnerbe/domain/runningitem/common/RunningItemType.kt
+++ domain/src/main/kotlin/team/applemango/runnerbe/domain/runningitem/common/RunningItemType.kt
@@ -17,14 +17,5 @@ package team.applemango.runnerbe.domain.runningitem.common
 enum class RunningItemType(val code: String) {
     Before("B"),
     After("A"),
-    Off("H");
-
-    /**
-     * 출근 전 - 06:00 AM, 퇴근 후 06:00 PM, 휴일 - 12:00 PM
-     */
-    fun toDefaultDateTime() = when (this) {
-        Before -> "06:00 AM"
-        After -> "06:00 PM"
-        Off -> "12:00 PM"
-    }
+    Off("H")
 }
