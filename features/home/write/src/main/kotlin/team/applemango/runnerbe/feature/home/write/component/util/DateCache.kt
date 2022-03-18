7971ce96b58e20e3f4be62b52ab46b14c91a5988
jisungbin <ji@sungb.in>
2022-03-18 09:56:29 +0900

    Add date caching class

---
+++ features/home/write/src/main/kotlin/team/applemango/runnerbe/feature/home/write/component/util/DateCache.kt
@@ -0,0 +1,28 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [DateCache.kt] created by Ji Sungbin on 22. 3. 18. 오전 9:37
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.feature.home.write.component.util
+
+import java.util.Calendar
+import java.util.Date
+import team.applemango.runnerbe.shared.domain.extension.toCalendar
+
+internal object DateCache {
+    private val cachedPlusDate = HashMap<Int, Date>()
+
+    fun Date.plusDayAndCaching(plus: Int): Date {
+        if (cachedPlusDate[plus] == null) {
+            val calendar = toCalendar().apply {
+                add(Calendar.DAY_OF_MONTH, plus)
+            }
+            cachedPlusDate[plus] = calendar.time
+        }
+        return cachedPlusDate[plus]!!
+    }
+}
