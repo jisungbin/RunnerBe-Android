3ac4b083586ed4a2f508fd2c616bd51e8afebb0c
jisungbin <ji@sungb.in>
2022-03-19 08:17:39 +0900

    Add new RangePicker module

---
+++ xml/rangepicker/build.gradle.kts
@@ -0,0 +1,28 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [build.gradle.kts] created by Ji Sungbin on 22. 3. 19. 오전 8:08
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+android {
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = Versions.Compose.Main
+    }
+}
+
+dependencies {
+    implementation(Dependencies.EachUi.Material)
+    Dependencies.Compose.forEach(::implementation)
+}
---
+++ xml/rangepicker/src/main/kotlin/team/applemango/runnerbe/xml/rangepicker/RangePicker.kt
@@ -0,0 +1,11 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [RangePicker.kt] created by Ji Sungbin on 22. 3. 19. 오전 8:14
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.xml.rangepicker
+
