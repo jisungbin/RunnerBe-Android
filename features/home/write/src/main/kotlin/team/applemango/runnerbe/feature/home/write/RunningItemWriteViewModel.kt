93531a6f58c53d91d10e0faa94207bf11e442dd1
jisungbin <ji@sungb.in>
2022-03-19 05:54:03 +0900

    Update RunningItemWriteViewModel variables

--- features/home/write/src/main/kotlin/team/applemango/runnerbe/feature/home/write/RunningItemWriteViewModel.kt
+++ features/home/write/src/main/kotlin/team/applemango/runnerbe/feature/home/write/RunningItemWriteViewModel.kt
@@ -11,10 +11,52 @@ package team.applemango.runnerbe.feature.home.write
 
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import team.applemango.runnerbe.domain.runningitem.common.RunningItemType
+import team.applemango.runnerbe.domain.runningitem.model.common.DefaultLocate
+import team.applemango.runnerbe.domain.runningitem.model.common.Locate
 import team.applemango.runnerbe.domain.runningitem.usecase.WriteRunningItemUseCase
+import team.applemango.runnerbe.feature.home.write.model.DefaultRunningTime
+import team.applemango.runnerbe.feature.home.write.model.RunningDate
+import team.applemango.runnerbe.feature.home.write.model.RunningTime
 import team.applemango.runnerbe.shared.base.BaseViewModel
+import team.applemango.runnerbe.shared.domain.constant.EmptyString
 
 @HiltViewModel
-class RunningItemWriteViewModel @Inject constructor(
+internal class RunningItemWriteViewModel @Inject constructor(
     private val writeRunningItemUseCase: WriteRunningItemUseCase,
-) : BaseViewModel()
+) : BaseViewModel() {
+    var locate = DefaultLocate
+        private set
+
+    var runningItemType = RunningItemType.Before
+        private set
+
+    var title = EmptyString
+        private set
+
+    var runningDate = RunningDate()
+        private set
+
+    var runningTime = DefaultRunningTime
+        private set
+
+    fun updateAddress(address: Locate) {
+        this.locate = address
+    }
+
+    fun updateRunningItemType(runningItemType: RunningItemType) {
+        this.runningItemType = runningItemType
+    }
+
+    fun updateTitle(title: String) {
+        this.title = title
+    }
+
+    fun updateRunningDate(runningDate: RunningDate) {
+        this.runningDate = runningDate
+    }
+
+    fun updateRunningTime(runningTime: RunningTime) {
+        this.runningTime = runningTime
+    }
+}
