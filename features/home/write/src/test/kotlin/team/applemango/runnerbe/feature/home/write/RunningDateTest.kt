1bfcef0aab65c1857600d0b4ee4e0618771942c4
jisungbin <ji@sungb.in>
2022-03-18 23:33:56 +0900

    Update RunningDate testing

--- features/home/write/src/test/kotlin/team/applemango/runnerbe/feature/home/write/RunningDateTest.kt
+++ features/home/write/src/test/kotlin/team/applemango/runnerbe/feature/home/write/RunningDateTest.kt
@@ -13,21 +13,14 @@ import java.util.Date
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import team.applemango.runnerbe.feature.home.write.component.util.DateCache.plusDayAndCaching
-import team.applemango.runnerbe.feature.home.write.constant.TimeType
 import team.applemango.runnerbe.feature.home.write.model.RunningDate
-import team.applemango.runnerbe.feature.home.write.model.toDateString
 
 class RunningDateTest {
     @Test
     @DisplayName("어제와 오늘의 RunningDate 를 비교했을 때 내일의 RunningDate 가 더 크게 나와야 함")
     fun compareToRunningDate() {
-        val today = RunningDate(
-            dateString = Date().toDateString(),
-            timeType = TimeType.AM,
-            hour = 1,
-            minute = 30
-        )
-        val tomorrow = Date().plusDayAndCaching(1)
+        val today = RunningDate()
+        val tomorrow = RunningDate(Date().plusDayAndCaching(1))
         assert(today < tomorrow)
     }
 }
