6d070fe9d114543dcac5e48c413c8d17ff3e8570
jisungbin <ji@sungb.in>
2022-03-18 23:19:28 +0900

    Create new RunningDate based on Calendar

--- features/home/write/src/main/kotlin/team/applemango/runnerbe/feature/home/write/model/RunningDate.kt
+++ features/home/write/src/main/kotlin/team/applemango/runnerbe/feature/home/write/model/RunningDate.kt
@@ -9,9 +9,9 @@
 
 package team.applemango.runnerbe.feature.home.write.model
 
-import java.text.SimpleDateFormat
+import androidx.annotation.IntRange
+import java.util.Calendar
 import java.util.Date
-import java.util.Locale
 import team.applemango.runnerbe.feature.home.write.constant.TimeType
 import team.applemango.runnerbe.shared.domain.extension.format
 
@@ -19,28 +19,57 @@ private const val RunningDateFormat = "M/dd (E)"
 
 internal fun Date.toDateString() = format(RunningDateFormat)
 
-internal data class RunningDate(
-    val dateString: String,
-    val timeType: TimeType,
-    val hour: Int,
-    val minute: Int,
-) {
-    private companion object {
-        const val FullDateFormat = "$RunningDateFormat a h m"
+internal class RunningDate(baseDate: Date = Date()) {
+    private val calendar = Calendar.getInstance().apply {
+        time = baseDate
     }
 
-    override fun toString() = "$dateString ${timeType.string} $hour $minute"
-
-    private fun toDate(): Date {
-        val date = SimpleDateFormat(
-            FullDateFormat,
-            Locale.getDefault()
-        ).parse(toString())
-        date ?: throw IllegalStateException("${toString()} is invalid format.")
-        return date
+    /**
+     * AM/PM 설정
+     *
+     * @param timeType AM/PM 값 (캘린더 인자는 AM - 0, PM - 1 사용)
+     */
+    fun setTimeType(timeType: TimeType) {
+        calendar.set(Calendar.AM_PM, TimeType.values().indexOf(timeType))
     }
 
-    operator fun compareTo(other: Date): Int {
-        return other.compareTo(toDate())
+    /**
+     * 시간 설정
+     *
+     * @param hour 1-12 시간대 (캘린더 인자는 0-11 시간대 사용)
+     */
+    fun setHour(hour: Int) {
+        calendar.set(Calendar.HOUR, hour - 1)
     }
+
+    /**
+     * 분 설정
+     *
+     * @param minute 분 (0 ~ 60)
+     */
+    fun setMinute(@IntRange(from = 0, to = 60) minute: Int) {
+        calendar.set(Calendar.MINUTE, minute)
+    }
+
+    /**
+     * 날짜 설정
+     *
+     * @param dateString 입력받는 포멧은 {월/일 (요일)} 이지만 월/일 만 사용됨
+     * 캘린더의 월은 0 부터 시작이라 -1 을 해서 캘린더에 넣음
+     */
+    fun setDate(dateString: String) {
+        val date = dateString.split(" ")[0]
+        val (month, day) = date.split("/").map(String::toInt)
+        calendar.set(Calendar.MONTH, month - 1)
+        calendar.set(Calendar.DAY_OF_MONTH, day)
+    }
+
+    /**
+     * Date 객체 반환
+     *
+     * @return Date
+     */
+    fun toDate() = calendar.time ?: throw NullPointerException("Can't get time from calendar.")
+
+    operator fun compareTo(other: Date) = toDate().compareTo(other)
 }
