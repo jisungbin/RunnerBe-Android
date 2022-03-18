cf31db491bff28681ac6ed0fc56fa83374ddeafa
jisungbin <ji@sungb.in>
2022-03-18 21:58:14 +0900

    Add regular typeface to Roboto

--- shared-compose/src/main/kotlin/team/applemango/runnerbe/shared/compose/theme/typography.kt
+++ shared-compose/src/main/kotlin/team/applemango/runnerbe/shared/compose/theme/typography.kt
@@ -41,6 +41,7 @@ object FontAsset {
 object FontTypeface {
     object Roboto {
         fun medium(context: Context) = ResourcesCompat.getFont(context, font.roboto_m)
+        fun regular(context: Context) = ResourcesCompat.getFont(context, font.roboto_r)
     }
 }
 
