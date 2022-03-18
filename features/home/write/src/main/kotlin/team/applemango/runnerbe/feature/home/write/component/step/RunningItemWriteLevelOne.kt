2ccf92bb5f58166efc62e7e380c74d88aa2c3257
jisungbin <ji@sungb.in>
2022-03-19 02:06:20 +0900

    Update RunningDatePickerDialog usage

--- features/home/write/src/main/kotlin/team/applemango/runnerbe/feature/home/write/component/step/RunningItemWriteLevelOne.kt
+++ features/home/write/src/main/kotlin/team/applemango/runnerbe/feature/home/write/component/step/RunningItemWriteLevelOne.kt
@@ -12,9 +12,7 @@ package team.applemango.runnerbe.feature.home.write.component.step
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -32,7 +30,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -42,43 +39,29 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
-import java.util.Calendar
-import java.util.Date
 import team.applemango.runnerbe.domain.runningitem.common.RunningItemType
 import team.applemango.runnerbe.feature.home.write.R
-import team.applemango.runnerbe.feature.home.write.util.DateCache.plusDayAndCaching
-import team.applemango.runnerbe.feature.home.write.constant.TimeType
+import team.applemango.runnerbe.feature.home.write.component.RunningDatePickerDialog
 import team.applemango.runnerbe.feature.home.write.model.RunningDate
-import team.applemango.runnerbe.feature.home.write.model.toDateString
-import team.applemango.runnerbe.shared.compose.component.RunnerbeDialog
-import team.applemango.runnerbe.shared.compose.default.RunnerbeSuperWheelPickerColors
-import team.applemango.runnerbe.shared.compose.default.RunnerbeSuperWheelPickerTextStyle
+import team.applemango.runnerbe.feature.home.write.model.RunningTime
+import team.applemango.runnerbe.feature.home.write.util.DateCache
 import team.applemango.runnerbe.shared.compose.theme.ColorAsset
 import team.applemango.runnerbe.shared.compose.theme.Typography
-import team.applemango.runnerbe.shared.domain.extension.toCalendar
-import team.applemango.runnerbe.shared.domain.unit.em
-import team.applemango.runnerbe.shared.domain.unit.px
 import team.applemango.runnerbe.shared.extension.collectWithLifecycle
-import team.applemango.runnerbe.xml.superwheelpicker.integration.SuperWheelPicker
 
 private val DefaultFieldShape = RoundedCornerShape(6.dp)
-
-private val now = Date()
 
 @Composable
 internal fun RunningItemWriteLevelOne(
     runningItemType: RunningItemType,
     fieldsAllInputStateChange: (state: Boolean) -> Unit,
 ) {
-    val runningDate = remember { RunningDate() }
+    val runningDate = remember { RunningDate.getDefault(runningItemType) }
     val lifecycleOwner = LocalLifecycleOwner.current
 
     var titleField by remember { mutableStateOf(TextFieldValue()) }
-    var runningDateStringState by remember { mutableStateOf<String?>(null) }
-    val runningDateStringDefaultState by remember {
-        mutableStateOf(RunningDate.getDefault(runningItemType).toString())
-    }
-    var runningTimeStringState by remember { mutableStateOf("0 시간 20 분") }
+    var runningDateState by remember { mutableStateOf(RunningDate.getDefault(runningItemType)) }
+    var runningTimeState by remember { mutableStateOf(RunningTime(hour = 0, minute = 20)) }
     val fieldsFillState = remember { mutableStateListOf(false, false, false) }
 
     var runningDatePickerDialogVisible by remember { mutableStateOf(false) }
@@ -95,6 +78,10 @@ internal fun RunningItemWriteLevelOne(
     RunningDatePickerDialog(
         visible = runningDatePickerDialogVisible,
         onDismissRequest = { runningDatePickerDialogVisible = false },
+        startDateIndex = DateCache.getCachedIndexFromDay(day = runningDate.getDay()),
+        startTimeType = runningDate.getTimeType(),
+        startHour = runningDate.getHour(),
+        startMinute = runningDate.getMinute(),
         onRunningDateChange = { field ->
             with(runningDate) {
                 when (field) {
@@ -190,7 +177,7 @@ internal fun RunningItemWriteLevelOne(
                         bottom.linkTo(parent.bottom)
                     }
                     .background(color = ColorAsset.G5_5),
-                text = runningDateStringState ?: runningDateStringDefaultState,
+                text = runningDateState.toString(),
                 style = Typography.Body14R.copy(color = ColorAsset.G1)
             )
             Icon(
@@ -239,7 +226,7 @@ internal fun RunningItemWriteLevelOne(
                         bottom.linkTo(parent.bottom)
                     }
                     .background(color = ColorAsset.G5_5),
-                text = runningTimeStringState,
+                text = runningTimeState.toString(),
                 style = Typography.Body14R.copy(color = ColorAsset.G1)
             )
             Icon(
@@ -257,138 +244,4 @@ internal fun RunningItemWriteLevelOne(
         }
         Divider(modifier = Modifier.padding(vertical = 20.dp), color = ColorAsset.G6)
     }
-}
-
-@Composable
-private fun RunningDatePickerDialog(
-    visible: Boolean,
-    onDismissRequest: () -> Unit,
-    onRunningDateChange: (field: RunningDate.Companion.Field) -> Unit,
-) {
-    RunnerbeDialog(
-        visible = visible,
-        onDismissRequest = onDismissRequest,
-        positiveButton = {
-            textBuilder = {
-                stringResource(R.string.runningitemwrite_dialog_button_decision)
-            }
-            onClick = {
-                onDismissRequest()
-            }
-        },
-        content = {
-            Row(
-                modifier = Modifier.matchParentSize(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                DateStringPicker { dateString ->
-                    onRunningDateChange(RunningDate.Companion.Field.Date(dateString))
-                }
-                TimeTypePicker { timeType ->
-                    onRunningDateChange(RunningDate.Companion.Field.TimeType(timeType))
-                }
-                Row {
-                    HourPicker { hour ->
-                        onRunningDateChange(RunningDate.Companion.Field.Hour(hour))
-                    }
-                    Text(text = ":", style = Typography.Custom.SuperWheelPicker)
-                    MinutePicker { minute ->
-                        onRunningDateChange(RunningDate.Companion.Field.Minute(minute))
-                    }
-                }
-            }
-        }
-    )
-}
-
-@Composable
-private fun DateStringPicker(
-    onDateStringSelectChange: (dateString: String) -> Unit,
-) {
-    SuperWheelPicker(
-        colors = RunnerbeSuperWheelPickerColors,
-        textStyle = RunnerbeSuperWheelPickerTextStyle.copy(
-            textSize = 18.px,
-            letterSpacing = (-0.18).em
-        ),
-        wheelItemCount = 5,
-        range = 0..6,
-        value = 0,
-        onValueChange = { _, dayUpper ->
-            val newDateString = now.plusDayAndCaching(dayUpper).toDateString()
-            onDateStringSelectChange(newDateString)
-        },
-        onTextRender = { value ->
-            now.plusDayAndCaching(value).toDateString()
-        }
-    )
-}
-
-@Composable
-private fun TimeTypePicker(
-    onTimeTypeSelectChange: (timeType: TimeType) -> Unit,
-) {
-    SuperWheelPicker(
-        colors = RunnerbeSuperWheelPickerColors,
-        textStyle = RunnerbeSuperWheelPickerTextStyle.copy(
-            textSize = 18.px,
-            letterSpacing = (-0.18).em
-        ),
-        wheelItemCount = 2,
-        range = 0..1,
-        value = 0,
-        onValueChange = { _, timeTypeIndex ->
-            onTimeTypeSelectChange(TimeType.values()[timeTypeIndex])
-        },
-        onTextRender = { value ->
-            TimeType.values()[value].string
-        }
-    )
-}
-
-@Composable
-private fun HourPicker(
-    onHourSelectChange: (hour: Int) -> Unit,
-) {
-    SuperWheelPicker(
-        colors = RunnerbeSuperWheelPickerColors,
-        textStyle = RunnerbeSuperWheelPickerTextStyle.copy(
-            textSize = 18.px,
-            letterSpacing = (-0.18).em
-        ),
-        wheelItemCount = 5,
-        range = 1..12,
-        value = now.toCalendar().get(Calendar.HOUR_OF_DAY),
-        onValueChange = { _, hour ->
-            onHourSelectChange(hour)
-        },
-    )
-}
-
-@Composable
-private fun MinutePicker(
-    onMinuteSelectChange: (hour: Int) -> Unit,
-) {
-    SuperWheelPicker(
-        colors = RunnerbeSuperWheelPickerColors,
-        textStyle = RunnerbeSuperWheelPickerTextStyle.copy(
-            textSize = 18.px,
-            letterSpacing = (-0.18).em
-        ),
-        wheelItemCount = 5,
-        range = 0..12,
-        value = now.toCalendar().get(Calendar.MINUTE),
-        onValueChange = { _, minute ->
-            onMinuteSelectChange(minute)
-        },
-        onTextRender = { value ->
-            val minute = value * 5
-            if (minute == 60) {
-                "0"
-            } else {
-                minute.toString()
-            }
-        }
-    )
 }
