8d36105d6cfbaeac02091ee27b44dd7b3ba37ded
jisungbin <ji@sungb.in>
2022-03-19 04:43:36 +0900

    Add locate extensions

---
+++ features/home/write/src/main/kotlin/team/applemango/runnerbe/feature/home/write/util/extension/locate.kt
@@ -0,0 +1,23 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [locate.kt] created by Ji Sungbin on 22. 3. 19. 오전 3:38
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.feature.home.write.util.extension
+
+import android.content.Context
+import android.location.Geocoder
+import com.google.android.gms.maps.model.LatLng
+import java.util.Locale
+import team.applemango.runnerbe.domain.runningitem.model.common.Locate
+
+internal fun Locate.toLatLng() = LatLng(latitude, longitude)
+internal fun LatLng.toAddress(context: Context): String {
+    val geoCoder = Geocoder(context, Locale.KOREA)
+    val addressList = geoCoder.getFromLocation(latitude, longitude, 2)
+    return addressList[1].getAddressLine(0).replace("대한민국 ", "")
+}
