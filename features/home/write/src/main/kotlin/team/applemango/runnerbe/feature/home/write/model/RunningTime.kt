7969d3926f4d8b2314efd453116fc90cad968940
jisungbin <ji@sungb.in>
2022-03-19 01:39:31 +0900

    Add RunningTime model

---
+++ features/home/write/src/main/kotlin/team/applemango/runnerbe/feature/home/write/model/RunningTime.kt
@@ -0,0 +1,12 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [RunningTime.kt] created by Ji Sungbin on 22. 3. 19. 오전 1:07
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.feature.home.write.model
+
+data class RunningTime(val hour: Int, val minute: Int)
