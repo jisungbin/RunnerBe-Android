166757d7136f15cec6cf3fb4042fc9b6bef5b53b
jisungbin <ji@sungb.in>
2022-03-19 09:35:18 +0900

    Add RunnerbeRangePickerDefaults

---
+++ shared-compose/src/main/kotlin/team/applemango/runnerbe/shared/compose/default/RunnerbeRangePickerDefaults.kt
@@ -0,0 +1,12 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [RunnerbeRangePickerDefaults.kt] created by Ji Sungbin on 22. 3. 19. 오전 9:33
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.shared.compose.default
+
+object RunnerbeRangePickerDefaults
