0d30e512d07b23fdf0f9b5c90a646ba533a8b021
jisungbin <ji@sungb.in>
2022-03-20 21:12:51 +0900

    Add new two extensions

--- shared/android/src/main/kotlin/team/applemango/runnerbe/shared/android/extension/activity.kt
+++ shared/android/src/main/kotlin/team/applemango/runnerbe/shared/android/extension/activity.kt
@@ -11,6 +11,12 @@ package team.applemango.runnerbe.shared.android.extension
 
 import android.app.Activity
 import android.content.Intent
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.core.view.WindowCompat
+import io.github.jisungbin.logeukes.LoggerType
+import io.github.jisungbin.logeukes.logeukes
+import team.applemango.runnerbe.shared.domain.extension.toMessage
 
 inline fun <reified T : Activity> Activity.changeActivityWithAnimation() {
     startActivity(Intent(this, T::class.java))
@@ -21,4 +27,20 @@ inline fun <reified T : Activity> Activity.changeActivityWithAnimation() {
 fun Activity.finishWithAnimation() {
     finish()
     overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+}
+
+fun Activity.setWindowInsets() {
+    window.setFlags( // 네비게이션바까지 영역 확장하려면 필요
+        WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+        WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+    )
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+}
+
+fun Activity.basicExceptionHandler(exception: Throwable) {
+    toast(
+        message = exception.toMessage(),
+        length = Toast.LENGTH_LONG
+    )
+    logeukes(type = LoggerType.E) { exception }
 }
