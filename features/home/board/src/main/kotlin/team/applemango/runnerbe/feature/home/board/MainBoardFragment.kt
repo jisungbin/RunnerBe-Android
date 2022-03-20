b386bb6ba3e3b8bc53786ef39465f71d8507d76e
jisungbin <ji@sungb.in>
2022-03-20 23:08:05 +0900

    Remove useless systemUiController

--- features/home/board/src/main/kotlin/team/applemango/runnerbe/feature/home/board/MainBoardFragment.kt
+++ features/home/board/src/main/kotlin/team/applemango/runnerbe/feature/home/board/MainBoardFragment.kt
@@ -2,6 +2,15 @@
  * RunnerBe © 2022 Team AppleMango. all rights reserved.
  * RunnerBe license is under the MIT.
  *
+ * [MainBoardFragment.kt] created by Ji Sungbin on 22. 3. 20. 오후 11:02
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
  * [MainBoardFragment.kt] created by Ji Sungbin on 22. 3. 15. 오후 6:48
  *
  * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
@@ -16,16 +25,14 @@ import android.view.ViewGroup
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.catch
@@ -52,16 +59,11 @@ class MainBoardFragment : Fragment() {
     ) = ComposeView(requireActivity()).apply {
         setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
         setContent {
-            val systemUiController = rememberSystemUiController()
-
-            LaunchedEffect(Unit) {
-                systemUiController.setStatusBarColor(color = Color.Transparent)
-            }
-
             MainBoardComposable(
                 modifier = Modifier
                     .fillMaxSize()
                     .background(brush = GradientAsset.BlackGradientBrush)
+                    .statusBarsPadding()
                     .padding(16.dp),
                 isBookmarkPage = arguments?.getBoolean("bookmark") ?: false,
                 isEmptyState = isEmptyState.collectAsState().value
@@ -72,13 +74,11 @@ class MainBoardFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         requireActivity().setWindowInsets()
-        vm.exceptionFlow
-            .catch { exception ->
-                requireActivity().basicExceptionHandler(exception)
-            }
-            .collectWithLifecycle(viewLifecycleOwner) { exception ->
-                requireActivity().basicExceptionHandler(exception)
-            }
+        vm.exceptionFlow.catch { exception ->
+            requireActivity().basicExceptionHandler(exception)
+        }.collectWithLifecycle(viewLifecycleOwner) { exception ->
+            requireActivity().basicExceptionHandler(exception)
+        }
         vm.observe(
             lifecycleOwner = this.viewLifecycleOwner,
             state = ::handleState,
