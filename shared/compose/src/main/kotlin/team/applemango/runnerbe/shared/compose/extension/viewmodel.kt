c2eae70c80f8fe19d998fda32950d3574948dd01
jisungbin <ji@sungb.in>
2022-03-21 14:21:07 +0900

    Add LocalActivity local provider

--- shared/compose/src/main/kotlin/team/applemango/runnerbe/shared/compose/extension/viewmodel.kt
+++ shared/compose/src/main/kotlin/team/applemango/runnerbe/shared/compose/extension/viewmodel.kt
@@ -13,14 +13,16 @@ import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
+import team.applemango.runnerbe.shared.compose.optin.LocalActivityUsageApi
 
+@LocalActivityUsageApi
 @Composable
 inline fun <reified VM : ViewModel> activityViewModel(
     key: String? = null,
     factory: ViewModelProvider.Factory? = null,
 ): VM = viewModel(
     VM::class.java,
-    getActivity(),
+    LocalActivity.current,
     key,
     factory
 )
