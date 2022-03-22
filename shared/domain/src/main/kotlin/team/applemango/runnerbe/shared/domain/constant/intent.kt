2c5af76808a5bdb16fae53a4166efc0ec0e4a63f
jisungbin <ji@sungb.in>
2022-03-22 23:28:22 +0900

    Add RunningItemId intent key constant

---
+++ shared/domain/src/main/kotlin/team/applemango/runnerbe/shared/domain/constant/intent.kt
@@ -0,0 +1,16 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [intent.kt] created by Ji Sungbin on 22. 3. 22. 오후 10:49
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.shared.domain.constant
+
+object Intent {
+    object Home {
+        const val RunningItemId = "intent-home-runningitemid"
+    }
+}
