3a13d45956fde79a42af7b1a56cb5c7b874d77e2
jisungbin <ji@sungb.in>
2022-03-23 10:22:38 +0900

    Update BoardDetail composable

--- features/home/detail/src/main/kotlin/team/applemango/runnerbe/feature/home/detail/component/BoardDetail.kt
+++ features/home/detail/src/main/kotlin/team/applemango/runnerbe/feature/home/detail/component/BoardDetail.kt
@@ -9,13 +9,30 @@
 
 package team.applemango.runnerbe.feature.home.detail.component
 
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import team.applemango.runnerbe.domain.runningitem.model.runningitem.information.RunningItemInformation
+import team.applemango.runnerbe.shared.compose.component.TopBar
 
 @Composable
 internal fun BoardDetail(
     modifier: Modifier = Modifier,
+    item: RunningItemInformation,
 ) {
     val context = LocalContext.current
+
+    TopBar(
+        startContent = {
+            Icon(
+                painter = ,
+                contentDescription = null
+            )
+        },
+        centerContent = {
+        },
+        endContent = {
+        }
+    )
 }
