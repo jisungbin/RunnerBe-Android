261ebf5497c913ae785522addaead0ff8f5cbfc6
jisungbin <ji@sungb.in>
2022-03-24 16:38:07 +0900

    Update RunningItem detail view screen

--- features/home/detail/src/main/kotlin/team/applemango/runnerbe/feature/home/detail/component/BoardDetail.kt
+++ features/home/detail/src/main/kotlin/team/applemango/runnerbe/feature/home/detail/component/BoardDetail.kt
@@ -11,14 +11,21 @@ package team.applemango.runnerbe.feature.home.detail.component
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
@@ -26,11 +33,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapUiSettings
 import team.applemango.runnerbe.domain.constant.Gender
 import team.applemango.runnerbe.domain.runningitem.model.runningitem.information.RunningItemInformation
 import team.applemango.runnerbe.feature.home.detail.DetailViewModel
@@ -55,6 +66,8 @@ private data class InformationItem(
     val text: String,
 )
 
+// TODO: 신청자 관리 (BottomSheet or LazyColumn 결정 필요)
+
 @OptIn(LocalActivityUsageApi::class) // LocalActivity.current, activityViewModel
 @Composable
 internal fun BoardDetail(
@@ -64,8 +77,11 @@ internal fun BoardDetail(
 ) {
     val activity = LocalActivity.current
 
-    val bookmarkIcon = remember(runningItemInformation.bookmarked) {
-
+    val bookmarkIconRes = remember(runningItemInformation.bookmarked) {
+        when (runningItemInformation.bookmarked) {
+            true -> R.drawable.ic_round_bookmark_24
+            else -> R.drawable.ic_outlined_bookmark_24
+        }
     }
 
     val informationItems = remember {
@@ -116,6 +132,7 @@ internal fun BoardDetail(
                     top.linkTo(parent.top)
                     bottom.linkTo(bottomCTAButtons.top, 50.dp)
                     width = Dimension.matchParent
+                    height = Dimension.fillToConstraints
                 }
         ) {
             TopBar(
@@ -150,7 +167,24 @@ internal fun BoardDetail(
                     )
                 }
             )
-            // MAP
+            GoogleMap(
+                modifier = Modifier
+                    .height(160.dp)
+                    .fillMaxWidth(),
+                uiSettings = MapUiSettings(
+                    indoorLevelPickerEnabled = false,
+                    mapToolbarEnabled = false,
+                    myLocationButtonEnabled = false,
+                    rotationGesturesEnabled = false,
+                    scrollGesturesEnabled = false,
+                    scrollGesturesEnabledDuringRotateOrZoom = false,
+                    tiltGesturesEnabled = false,
+                    zoomControlsEnabled = false,
+                    zoomGesturesEnabled = false,
+                )
+            ) {
+                // TODO: Marker, Circle
+            }
             CircleBorderText(
                 modifier = Modifier.padding(
                     top = 24.dp,
@@ -174,7 +208,8 @@ internal fun BoardDetail(
             LazyColumn(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .wrapContentHeight()
+                    .wrapContentHeight(),
+                verticalArrangement = Arrangement.spacedBy(2.dp)
             ) {
                 items(items = informationItems) { information ->
                     IconText(
@@ -203,14 +238,47 @@ internal fun BoardDetail(
                     bottom.linkTo(parent.bottom)
                 }
                 .background(color = ColorAsset.G6)
+                .padding(
+                    vertical = 8.dp,
+                    horizontal = 16.dp
+                )
         ) {
             val (bookmark, join) = createRefs()
 
             Icon(
-                modifier = Modifier,
-                painter =,
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .border(
+                        color = ColorAsset.Primary,
+                        width = 1.dp,
+                        shape = CircleShape
+                    )
+                    .constrainAs(bookmark) {
+                        start.linkTo(parent.start)
+                        width = Dimension.value(40.dp)
+                    }.clickable {
+                        // TODO: update bookmark state
+                    },
+                painter = painterResource(bookmarkIconRes),
                 contentDescription = null
             )
+            Button(
+                modifier = Modifier
+                    .constrainAs(join) {
+                        start.linkTo(bookmark.end, 12.dp)
+                        width = Dimension.value(40.dp)
+                    }
+                    .clip(RoundedCornerShape(24.dp)),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = ColorAsset.Primary
+                ),
+                onClick = { /*TODO*/ }
+            ) {
+                Text(
+                    text = stringResource(R.string.detail_button_register),
+                    style = Typography.Body14M.copy(color = Color.Black)
+                )
+            }
         }
     }
 }
