dc6dfcb67816804c0496846cd826ee12a15489c7
jisungbin <ji@sungb.in>
2022-03-26 14:20:59 +0900

    Add RunningItemsState model

---
+++ features/home/board/src/main/kotlin/team/applemango/runnerbe/feature/home/board/RunningItemsState.kt
@@ -0,0 +1,19 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [RunningItemsState.kt] created by Ji Sungbin on 22. 3. 26. 오후 2:11
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.feature.home.board
+
+import team.applemango.runnerbe.domain.runningitem.model.runningitem.RunningItem
+
+internal sealed class RunningItemsState {
+    object Loading : RunningItemsState()
+    object LoadFail : RunningItemsState()
+    object Empty : RunningItemsState()
+    data class Loaded(val items: List<RunningItem>) : RunningItemsState()
+}
