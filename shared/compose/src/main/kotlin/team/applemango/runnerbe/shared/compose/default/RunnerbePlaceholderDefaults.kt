1f91ec4a73e87227d4eae440b19bc88cc1890832
jisungbin <ji@sungb.in>
2022-03-26 23:45:19 +0900

    Add RunnerbePlaceholderDefaults

---
+++ shared/compose/src/main/kotlin/team/applemango/runnerbe/shared/compose/default/RunnerbePlaceholderDefaults.kt
@@ -0,0 +1,21 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [RunnerbePlaceholderDefaults.kt] created by Ji Sungbin on 22. 3. 26. 오후 11:18
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.shared.compose.default
+
+import androidx.compose.runtime.Stable
+import team.applemango.runnerbe.shared.compose.theme.ColorAsset
+
+object RunnerbePlaceholderDefaults {
+    @Stable
+    val BaseColor = ColorAsset.G5_5
+
+    @Stable
+    val HighlightColor = ColorAsset.G5
+}
