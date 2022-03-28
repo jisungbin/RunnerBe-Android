d92387faba2524116c8635546d0c140413b47d81
jisungbin <ji@sungb.in>
2022-03-29 01:23:08 +0900

    Change RegisterResponse models

---
+++ data/src/main/kotlin/team/applemango/runnerbe/data/register/login/model/register/Result.kt
@@ -0,0 +1,20 @@
+/*
+ * RunnerBe © 2022 Team AppleMango. all rights reserved.
+ * RunnerBe license is under the MIT.
+ *
+ * [Result.kt] created by Ji Sungbin on 22. 3. 29. 오전 1:20
+ *
+ * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
+ */
+
+package team.applemango.runnerbe.data.register.login.model.register
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class Result(
+    @field:JsonProperty("insertedUserId")
+    val insertedUserId: Int? = null,
+
+    @field:JsonProperty("token")
+    val token: String? = null,
+)
