7c2397241e8d789adfc9893a1975cf62b0347319
jisungbin <ji@sungb.in>
2022-03-29 01:50:53 +0900

    Remove useless splash preDrawListener and rootView variable, and save UserToken

--- presentation/src/main/kotlin/team/applemango/runnerbe/activity/StartActivity.kt
+++ presentation/src/main/kotlin/team/applemango/runnerbe/activity/StartActivity.kt
@@ -11,14 +11,15 @@ package team.applemango.runnerbe.activity
 
 import android.os.Build
 import android.os.Bundle
-import android.view.ViewTreeObserver
 import android.view.animation.AnticipateInterpolator
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.first
+import team.applemango.runnerbe.domain.register.runnerbe.model.UserToken
 import team.applemango.runnerbe.shared.android.constant.DataStoreKey
+import team.applemango.runnerbe.shared.android.datastore.Me
 import team.applemango.runnerbe.shared.android.extension.basicExceptionHandler
 import team.applemango.runnerbe.shared.android.extension.changeActivityWithAnimation
 import team.applemango.runnerbe.shared.android.extension.dataStore
@@ -34,14 +35,11 @@ class StartActivity : AppCompatActivity() {
         const val SplashAnimationDuration = 200L
     }
 
-    private val rootView by lazy { LinearLayout(this) }
-
-    @Suppress("KotlinConstantConditions") // `!isSnsLoginDone` is always true
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(
-            rootView,
+            LinearLayout(this),
             LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.MATCH_PARENT
@@ -55,37 +53,34 @@ class StartActivity : AppCompatActivity() {
                 .defaultCatch(action = ::basicExceptionHandler)
                 .first()
 
-            val isSignedUser = preferences[DataStoreKey.Login.Jwt] != null
-            val isSnsLoginDone = preferences[DataStoreKey.Login.Uuid] != null
+            val userId = preferences[DataStoreKey.Login.UserId]
+            val userJwt = preferences[DataStoreKey.Login.Jwt]
+            val userUuid = preferences[DataStoreKey.Login.Uuid]
             val isUnregistered = preferences[DataStoreKey.Onboard.Unregister] == true
             if (isUnregistered) {
                 changeActivityWithAnimation<MainActivity>()
                 return@launchedWhenCreated
             }
+            @Suppress("SENSELESS_COMPARISON") // `userUuid == null` always true
             when {
-                isSignedUser -> { // JWT 존재
+                userJwt != null && userId != null -> { // 회원가입 완료
+                    Me.token = UserToken(
+                        userId = userId,
+                        jwt = userJwt
+                    )
                     changeActivityWithAnimation<MainActivity>()
                     return@launchedWhenCreated
                 }
-                isSnsLoginDone -> { // SNS 로그인 완료 -> 온보딩 페이지로 이동
+                userUuid != null -> { // SNS 로그인 완료 -> 온보딩 페이지로 이동
                     changeActivityWithAnimation<DFMOnboardActivityAlias>()
                     return@launchedWhenCreated
                 }
-                !isSnsLoginDone -> { // SNS 로그인 하기 전
+                userUuid == null -> { // SNS 로그인 하기 전 (앱 처음 실행)
                     changeActivityWithAnimation<DFMLoginActivityAlias>()
                     return@launchedWhenCreated
                 }
             }
         }
-
-        rootView.viewTreeObserver.addOnPreDrawListener(
-            object : ViewTreeObserver.OnPreDrawListener {
-                override fun onPreDraw(): Boolean {
-                    rootView.viewTreeObserver.removeOnPreDrawListener(this)
-                    return true
-                }
-            }
-        )
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             splashScreen.setOnExitAnimationListener { splashScreenView ->
