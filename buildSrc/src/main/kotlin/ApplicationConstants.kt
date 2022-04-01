1620d1048947a23775c5fe634292658c2e2a2694
jisungbin <ji@sungb.in>
2022-04-02 02:06:36 +0900

    Chore: increase app version code

--- buildSrc/src/main/kotlin/ApplicationConstants.kt
+++ buildSrc/src/main/kotlin/ApplicationConstants.kt
@@ -14,7 +14,7 @@ object ApplicationConstants {
     const val targetSdk = 31
     const val compileSdk = 31
     const val jvmTarget = "11"
-    const val versionCode = 9
+    const val versionCode = 10
     const val versionName = "0.1.0-dev10"
     val javaVersion = JavaVersion.VERSION_11
 }
