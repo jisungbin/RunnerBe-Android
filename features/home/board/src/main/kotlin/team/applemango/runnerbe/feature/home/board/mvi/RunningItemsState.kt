90327b78d499ddebe247ab0dcc6457bc807739e4
jisungbin <ji@sungb.in>
2022-04-01 23:43:17 +0900

    Add comment to RunningItemsState

--- features/home/board/src/main/kotlin/team/applemango/runnerbe/feature/home/board/mvi/RunningItemsState.kt
+++ features/home/board/src/main/kotlin/team/applemango/runnerbe/feature/home/board/mvi/RunningItemsState.kt
@@ -11,6 +11,9 @@ package team.applemango.runnerbe.feature.home.board.mvi
 
 import team.applemango.runnerbe.domain.runningitem.model.runningitem.RunningItem
 
+/**
+ * 러닝 아이템 조회 상태
+ */
 internal sealed class RunningItemsState {
     object Loading : RunningItemsState()
     object LoadFail : RunningItemsState()
