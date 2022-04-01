f00ad5c37c2663d590c36967fb27671f9d9af4d7
jisungbin <ji@sungb.in>
2022-04-01 23:40:22 +0900

    Remove snackbar test Row composable

--- features/home/board/src/main/kotlin/team/applemango/runnerbe/feature/home/board/MainBoardFragment.kt
+++ features/home/board/src/main/kotlin/team/applemango/runnerbe/feature/home/board/MainBoardFragment.kt
@@ -14,34 +14,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.MutableStateFlow
 import org.orbitmvi.orbit.viewmodel.observe
 import team.applemango.runnerbe.domain.constant.Gender
 import team.applemango.runnerbe.domain.runningitem.common.RunningItemType
@@ -67,7 +54,6 @@ import team.applemango.runnerbe.shared.domain.extension.defaultCatch
 class MainBoardFragment : Fragment() {
 
     private val vm: MainBoardViewModel by activityViewModels()
-    private val isEmptyState = MutableStateFlow(false)
 
     @OptIn(LocalActivityUsageApi::class) // LocalActivity usage
     override fun onCreateView(
@@ -104,7 +90,7 @@ class MainBoardFragment : Fragment() {
                                 runningItemsState = RunningItemsState.Loading
                             }
                             MainBoardState.RunningItemLoadFail -> {
-                                runningItemsState = RunningItemsState.Loaded(emptyList())
+                                runningItemsState = RunningItemsState.LoadFail
                             }
                             is MainBoardState.RunningItemLoaded -> {
                                 val items = state.items
@@ -126,35 +112,18 @@ class MainBoardFragment : Fragment() {
             }
 
             CompositionLocalProvider(LocalActivity provides requireActivity()) {
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.BottomCenter
-                ) {
-                    MainBoardComposable(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .background(brush = GradientAsset.Background.Brush)
-                            .statusBarsPadding() // without navigationBar: because use BottomSheetView inner
-                            .zIndex(1f),
-                        runningItems = (runningItemsState as? RunningItemsState.Loaded)?.items
-                            ?: emptyList(),
-                        isLoading = runningItemsState == RunningItemsState.Loading,
-                        isBookmarkPage = arguments?.getBoolean("bookmark") ?: false,
-                        isEmptyState = runningItemsState == RunningItemsState.Empty
-                    )
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentHeight()
-                            .clip(RoundedCornerShape(10.dp))
-                            .background(color = Color.Red)
-                            .blur(10.dp)
-                            .padding(horizontal = 16.dp)
-                            .zIndex(2f)
-                    ) {
-                        Text(text = "text")
-                    }
-                }
+                MainBoardComposable(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(brush = GradientAsset.Background.Brush)
+                        .statusBarsPadding() // without navigationBar: because use BottomSheetView inner
+                        .zIndex(1f),
+                    runningItems = (runningItemsState as? RunningItemsState.Loaded)?.items
+                        ?: emptyList(),
+                    isLoading = runningItemsState == RunningItemsState.Loading,
+                    isBookmarkPage = arguments?.getBoolean("bookmark") ?: false,
+                    isEmptyState = runningItemsState == RunningItemsState.Empty
+                )
             }
         }
     }
