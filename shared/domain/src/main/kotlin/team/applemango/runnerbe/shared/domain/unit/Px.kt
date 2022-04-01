d19224589ec42bd065da77220c07c16373dc7f49
jisungbin <ji@sungb.in>
2022-04-01 15:59:16 +0900

    Update toInt method

--- shared/domain/src/main/kotlin/team/applemango/runnerbe/shared/domain/unit/Px.kt
+++ shared/domain/src/main/kotlin/team/applemango/runnerbe/shared/domain/unit/Px.kt
@@ -10,4 +10,7 @@
 package team.applemango.runnerbe.shared.domain.unit
 
 @JvmInline
-value class Px(val value: Float)
+value class Px(val value: Float) {
+    // The adding of 0.5 is used to round UP to the nearest integer value.
+    fun toInt() = (value + 0.5f).toInt()
+}
