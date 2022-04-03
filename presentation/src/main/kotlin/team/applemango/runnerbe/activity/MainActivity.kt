aeef06fe5dcbfd31961d80319abe7a24d5282941
jisungbin <ji@sungb.in>
2022-04-03 23:21:41 +0900

    Update ModalBottomSheetLayout usage

--- presentation/src/main/kotlin/team/applemango/runnerbe/activity/MainActivity.kt
+++ presentation/src/main/kotlin/team/applemango/runnerbe/activity/MainActivity.kt
@@ -15,34 +15,29 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.rememberModalBottomSheetState
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import com.birjuvachhani.locus.Locus
 import dagger.hilt.android.AndroidEntryPoint
 import team.applemango.runnerbe.R
-import team.applemango.runnerbe.component.ModalBottomSheetContentValue
-import team.applemango.runnerbe.component.rememberModalBottomSheetContentState
 import team.applemango.runnerbe.constant.Screen
 import team.applemango.runnerbe.shared.android.datastore.Me
-import team.applemango.runnerbe.shared.android.extension.collectWithLifecycle
 import team.applemango.runnerbe.shared.android.extension.setWindowInsetsUsage
 import team.applemango.runnerbe.shared.compose.component.BottomBar
 import team.applemango.runnerbe.shared.compose.component.BottomBarItem
@@ -103,59 +98,29 @@ class MainActivity : AppCompatActivity() {
         setWindowInsetsUsage()
         setContent {
             val modalBottomSheetState = rememberModalBottomSheetState(
-                initialValue = ModalBottomSheetValue.Hidden,
+                initialValue = ModalBottomSheetValue.Expanded,
                 skipHalfExpanded = true
             )
-            val modalBottomSheetContentState = rememberModalBottomSheetContentState(
-                initialValue = ModalBottomSheetContentValue.Hidden
-            )
-            var statusBarColor by remember { mutableStateOf(Color.Transparent) }
-
-            LaunchedEffect(Unit) {
-                snapshotFlow { modalBottomSheetContentState.state }
-                    .collectWithLifecycle(this@MainActivity) { state ->
-                        when (state) {
-                            ModalBottomSheetContentValue.Hidden -> {
-                                modalBottomSheetState.hide()
-                            }
-                            is ModalBottomSheetContentValue.Expand -> {
-                                modalBottomSheetState.show()
-                            }
-                        }
-                    }
-            }
 
             ModalBottomSheetLayout(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = ColorAsset.G6),
                 sheetState = modalBottomSheetState,
                 sheetShape = RoundedCornerShape(
                     topStart = 30.dp,
                     topEnd = 30.dp
                 ),
-                sheetBackgroundColor = ColorAsset.G6,
+                sheetBackgroundColor = Color.Cyan,
                 scrimColor = Color.Black.copy(alpha = 0.5f),
                 sheetContent = {
-                    when (val state = modalBottomSheetContentState.state) {
-                        ModalBottomSheetContentValue.Hidden -> Box(modifier = Modifier.fillMaxSize())
-                        is ModalBottomSheetContentValue.Expand -> state.content()
-                    }
+                    Box(modifier = Modifier.fillMaxWidth().height(300.dp))
                 },
             ) {
                 ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-                    val (statusBarColorRef, contentRef, bottomBarRef) = createRefs()
-                    val statusBarHeight = WindowInsets.statusBars.getTop(LocalDensity.current).dp
+                    val (contentRef, bottomBarRef) = createRefs()
                     var screenState by remember { mutableStateOf(Screen.Home) }
 
-                    Box(
-                        modifier = Modifier
-                            .constrainAs(statusBarColorRef) {
-                                top.linkTo(anchor = parent.top)
-
-                                width = Dimension.matchParent
-                                height = Dimension.value(statusBarHeight)
-                            }
-                            .background(color = statusBarColor)
-                    )
                     Crossfade(
                         modifier = Modifier
                             .constrainAs(contentRef) {
@@ -183,11 +148,12 @@ class MainActivity : AppCompatActivity() {
                         }
                     }
                     BottomBar(
-                        modifier = Modifier.constrainAs(bottomBarRef) {
-                            bottom.linkTo(anchor = parent.bottom)
-
-                            width = Dimension.matchParent
-                        },
+                        modifier = Modifier
+                            .navigationBarsPadding()
+                            .constrainAs(bottomBarRef) {
+                                bottom.linkTo(anchor = parent.bottom)
+                                width = Dimension.matchParent
+                            },
                         colors = RunnerbeBottomBarDefaults.colors(),
                         items = bottomBarItems
                     ) { selectedItem ->
