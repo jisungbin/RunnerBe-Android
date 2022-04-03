5f159f023b2ce66281c67b40b53c88e0a7c6bbbf
jisungbin <ji@sungb.in>
2022-04-04 00:20:16 +0900

    Add height constant to RunnerbeBottomBarDefaults

--- shared/compose/src/main/kotlin/team/applemango/runnerbe/shared/compose/default/RunnerbeBottomBarDefaults.kt
+++ shared/compose/src/main/kotlin/team/applemango/runnerbe/shared/compose/default/RunnerbeBottomBarDefaults.kt
@@ -2,10 +2,14 @@ package team.applemango.runnerbe.shared.compose.default
 
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import team.applemango.runnerbe.shared.compose.component.BottomBarColors
 import team.applemango.runnerbe.shared.compose.theme.ColorAsset
 
 object RunnerbeBottomBarDefaults {
+    @Stable
+    val height = 60.dp
+
     @Stable
     fun colors() = BottomBarColors(
         background = ColorAsset.G6,
