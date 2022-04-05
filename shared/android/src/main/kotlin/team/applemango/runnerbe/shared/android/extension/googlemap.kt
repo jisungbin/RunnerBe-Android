be15d588c9a94faee6a02deaa8c81df2ab2daf6b
jisungbin <ji@sungb.in>
2022-04-05 20:18:31 +0900

    Move map related utils to googlemap.kt shared util

--- shared/android/src/main/kotlin/team/applemango/runnerbe/shared/android/extension/googlemap.kt
+++ shared/android/src/main/kotlin/team/applemango/runnerbe/shared/android/extension/googlemap.kt
@@ -2,23 +2,35 @@
  * RunnerBe © 2022 Team AppleMango. all rights reserved.
  * RunnerBe license is under the MIT.
  *
- * [bitmapDescriptorFromVector.kt] created by Ji Sungbin on 22. 4. 5. 오후 5:07
+ * [googlemap.kt] created by Ji Sungbin on 22. 3. 19. 오전 3:38
  *
  * Please see: https://github.com/applemango-runnerbe/RunnerBe-Android/blob/main/LICENSE.
  */
 
-package team.applemango.runnerbe.feature.home.filter.extension
+package team.applemango.runnerbe.shared.android.extension
 
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.location.Geocoder
 import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import team.applemango.runnerbe.domain.runningitem.model.common.Locate
+import java.util.Locale
+
+fun Locate.toLatLng() = LatLng(latitude, longitude)
+
+fun LatLng.toAddress(context: Context): String {
+    val geoCoder = Geocoder(context, Locale.KOREA)
+    val addressList = geoCoder.getFromLocation(latitude, longitude, 2)
+    return addressList[1].getAddressLine(0).replace("대한민국 ", "")
+}
 
 // https://stackoverflow.com/a/45564994/14299073
-internal fun Context.bitmapDescriptorFromVector(
+fun Context.bitmapDescriptorFromVector(
     @DrawableRes vectorResId: Int
 ): BitmapDescriptor? {
     return ContextCompat.getDrawable(this, vectorResId)?.run {
