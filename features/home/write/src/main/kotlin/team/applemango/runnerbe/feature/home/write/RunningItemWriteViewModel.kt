50bc6c787f9465e45324a0e5b27b9d9aef9c5844
jisungbin <ji@sungb.in>
2022-04-06 03:14:36 +0900

    Update RunningItemWriteViewModel

--- features/home/write/src/main/kotlin/team/applemango/runnerbe/feature/home/write/RunningItemWriteViewModel.kt
+++ features/home/write/src/main/kotlin/team/applemango/runnerbe/feature/home/write/RunningItemWriteViewModel.kt
@@ -11,14 +11,19 @@ package team.applemango.runnerbe.feature.home.write
 
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.reduce
 import org.orbitmvi.orbit.viewmodel.container
+import team.applemango.runnerbe.domain.constant.Gender
 import team.applemango.runnerbe.domain.runningitem.common.RunningItemType
+import team.applemango.runnerbe.domain.runningitem.filter.AgeFilter
+import team.applemango.runnerbe.domain.runningitem.model.runningitem.RunningItemApiBody
 import team.applemango.runnerbe.domain.runningitem.usecase.WriteRunningItemUseCase
 import team.applemango.runnerbe.feature.home.write.model.DefaultRunningTime
 import team.applemango.runnerbe.feature.home.write.model.RunningDate
-import team.applemango.runnerbe.feature.home.write.model.RunningTime
 import team.applemango.runnerbe.feature.home.write.mvi.RunningItemWriteState
 import team.applemango.runnerbe.shared.android.base.BaseViewModel
+import team.applemango.runnerbe.shared.android.datastore.Me
 import team.applemango.runnerbe.shared.domain.constant.EmptyString
 import javax.inject.Inject
 
@@ -29,31 +34,43 @@ internal class RunningItemWriteViewModel @Inject constructor(
 
     override val container = container<RunningItemWriteState, Nothing>(RunningItemWriteState.None)
 
-    var runningItemType = RunningItemType.Before
-        private set
-
     var title = EmptyString
-        private set
-
+    var message = EmptyString
+    var gender = Gender.All
+    var maxPeopleCount = 0
+    var ageFilter = AgeFilter.None
+    var itemType = RunningItemType.Before
     var runningDate = RunningDate()
-        private set
-
     var runningTime = DefaultRunningTime
-        private set
+    var locate = Me.locate.value
 
-    fun updateRunningItemType(runningItemType: RunningItemType) {
-        this.runningItemType = runningItemType
-    }
-
-    fun updateTitle(title: String) {
-        this.title = title
-    }
-
-    fun updateRunningDate(runningDate: RunningDate) {
-        this.runningDate = runningDate
-    }
-
-    fun updateRunningTime(runningTime: RunningTime) {
-        this.runningTime = runningTime
+    fun writeRunningItem() = intent {
+        val jwt = checkNotNull(Me.token.jwt) {
+            "User must be registered."
+        }
+        val userId = checkNotNull(Me.token.userId) {
+            "User must be registered."
+        }
+        writeRunningItemUseCase(
+            jwt = jwt,
+            userId = userId,
+            item = RunningItemApiBody(
+                title = title,
+                itemType = itemType,
+                meetingDate = runningDate.toDate(),
+                runningTime = runningTime.toString(withWhitespace = false),
+                locate = locate,
+                ageFilter = ageFilter,
+                maxPeopleCount = maxPeopleCount,
+                gender = gender,
+                message = message
+            )
+        ).onSuccess {
+            reduce {
+                RunningItemWriteState.Done
+            }
+        }.onFailure { exception ->
+            emitException(exception)
+        }
     }
 }
