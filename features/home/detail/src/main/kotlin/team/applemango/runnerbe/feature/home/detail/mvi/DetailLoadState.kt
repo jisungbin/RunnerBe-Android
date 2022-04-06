4c93c7242b75fd0690d8e10471c105761cd0f344
jisungbin <ji@sungb.in>
2022-04-06 22:04:49 +0900

    Cleanup DetailActivity mvi model

--- features/home/detail/src/main/kotlin/team/applemango/runnerbe/feature/home/detail/mvi/DetailLoadState.kt
+++ features/home/detail/src/main/kotlin/team/applemango/runnerbe/feature/home/detail/mvi/DetailLoadState.kt
@@ -9,15 +9,17 @@
 
 package team.applemango.runnerbe.feature.home.detail.mvi
 
+import team.applemango.runnerbe.domain.runningitem.model.runningitem.information.RunningItemInformation
+import team.applemango.runnerbe.feature.home.detail.mvi.DetailLoadState.Loaded
+import team.applemango.runnerbe.feature.home.detail.mvi.DetailLoadState.Loading
+
 /**
  * 러닝 아이템 상세 조회 상태
  *
- * @property Load 로딩 중
- * @property Done 로드 완료
- * @property NotVerified 아직 인증되지 않은 계정
+ * @property Loading 로딩 중
+ * @property Loaded 로드 완료
  */
-internal enum class DetailLoadState {
-    Load,
-    Done,
-    NotVerified
+internal sealed class DetailLoadState {
+    object Loading : DetailLoadState()
+    data class Loaded(val item: RunningItemInformation) : DetailLoadState()
 }
