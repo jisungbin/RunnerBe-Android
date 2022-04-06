7a71588a796718184bab8d35ed288c8817400d82
jisungbin <ji@sungb.in>
2022-04-06 21:57:42 +0900

    Add Activity.startActivityWithAnimation extension

--- shared/android/src/main/kotlin/team/applemango/runnerbe/shared/android/extension/activity.kt
+++ shared/android/src/main/kotlin/team/applemango/runnerbe/shared/android/extension/activity.kt
@@ -21,9 +21,19 @@ import team.applemango.runnerbe.shared.domain.extension.toMessage
 inline fun <reified T : Activity> Activity.changeActivityWithAnimation(
     intentBuilder: Intent.() -> Intent = { this }
 ) {
+    startActivityWithAnimation<T>(
+        intentBuilder = intentBuilder,
+        withFinish = true
+    )
+}
+
+inline fun <reified T : Activity> Activity.startActivityWithAnimation(
+    intentBuilder: Intent.() -> Intent = { this },
+    withFinish: Boolean = false
+) {
     startActivity(intentBuilder(Intent(this, T::class.java)))
     overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-    finish()
+    if (withFinish) finish()
 }
 
 fun Activity.finishWithAnimation() {
